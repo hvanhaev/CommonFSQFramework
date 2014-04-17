a6c8dc04c92c49d3a976d7a6ba35ead16ba4c3ef
Tomasz <fruboes@cern.ch>
2014-04-17 19:04:07 +0200

    Add float branches

--- plugins/ExampleTreeProducer.cc
+++ plugins/ExampleTreeProducer.cc
@@ -18,6 +18,8 @@
 //
 
 
+
+
 // system include files
 #include <memory>
 
@@ -33,6 +35,8 @@
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 #include "TTree.h"
+
+#include "DataFormats/PatCandidates/interface/Jet.h"
 //
 // class declaration
 //
@@ -53,6 +57,7 @@ class ExampleTreeProducer : public edm::EDAnalyzer {
 
       TTree *m_tree;
       std::map<std::string, int> m_integerBranches;
+      std::map<std::string, float> m_floatBranches;
 
 
       //virtual void beginRun(edm::Run const&, edm::EventSetup const&) override;
@@ -82,25 +87,63 @@ ExampleTreeProducer::ExampleTreeProducer(const edm::ParameterSet& iConfig)
     m_tree = tFileService->make<TTree>("data", "data");
 
     // define your branches. Registration will be made automagically
+    //
+    //  note: verify, that your data is filled in the tree. If you will
+    //        define your variable name as m_floatBranches["leadJetPt"]
+    //        and than missspell the name in the analyze method:
+    //          m_floatBranches["leadJetPT"] = 123; // note the wrong case in PT/Pt
+    //        program will continue to run not filling your variable
+    //
+    // use m_integerBranches for integer values
     m_integerBranches["run"] = 0;
     m_integerBranches["lumi"] = 0;
     m_integerBranches["event"] = 0;
 
+    // use m_floatBranches for float values
+    m_floatBranches["leadJetPt"] = 0;
+    m_floatBranches["leadJetEta"] = 0;
+    m_floatBranches["subleadJetPt"] = 0;
+    m_floatBranches["subleadJetEta"] = 0;
 
-    std::map<std::string, int>::iterator it =  m_integerBranches.begin();
-    std::map<std::string, int>::iterator itE =  m_integerBranches.end();
-    for (;it != itE;++it){
-        m_tree->Branch(it->first.c_str(), &it->second, (it->first+"/I").c_str());
+    // integer branches auto registration
+    {
+        std::map<std::string, int>::iterator it =  m_integerBranches.begin();
+        std::map<std::string, int>::iterator itE =  m_integerBranches.end();
+        for (;it != itE;++it){
+            m_tree->Branch(it->first.c_str(), &it->second, (it->first+"/I").c_str());
+        }
     }
 
-   //now do what ever initialization is needed
+    // float branches auto registration
+    {
+        std::map<std::string, float>::iterator it =  m_floatBranches.begin();
+        std::map<std::string, float>::iterator itE =  m_floatBranches.end();
+        for (;it != itE;++it){
+            m_tree->Branch(it->first.c_str(), &it->second, (it->first+"/F").c_str());
+        }
+
+    }
+
 
 }
 void ExampleTreeProducer::resetTrees(){
-    std::map<std::string, int>::iterator it =  m_integerBranches.begin();
-    std::map<std::string, int>::iterator itE =  m_integerBranches.end();
-    for (;it != itE;++it){
-            m_integerBranches[it->first]=0;
+
+    // int branches
+    {
+        std::map<std::string, int>::iterator it =  m_integerBranches.begin();
+        std::map<std::string, int>::iterator itE =  m_integerBranches.end();
+        for (;it != itE;++it){
+                m_integerBranches[it->first]=0;
+        }
+    }
+
+    // float branches
+    {
+        std::map<std::string, float>::iterator it =  m_floatBranches.begin();
+        std::map<std::string, float>::iterator itE =  m_floatBranches.end();
+        for (;it != itE;++it){
+                m_floatBranches[it->first]=0;
+        }
     }
 
 
@@ -124,13 +167,39 @@ ExampleTreeProducer::~ExampleTreeProducer()
 void
 ExampleTreeProducer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-    using namespace edm;
     resetTrees();
+    using namespace edm;
 
     //std::cout << "Moin Agatko!" << std::endl;
     m_integerBranches["run"] = iEvent.eventAuxiliary().run();
     m_integerBranches["lumi"] = iEvent.eventAuxiliary().luminosityBlock();
     m_integerBranches["event"] = iEvent.eventAuxiliary().event();
+
+    edm::Handle<pat::JetCollection> hJets;
+    iEvent.getByLabel(edm::InputTag("patJets"), hJets);  // TODO/Fixme - inputTag from python cfg
+
+    /*
+    for (unsigned int i = 0; i<hJets->size(); ++i){
+        std::cout   << "Jet:"  
+                    << " " <<  hJets->at(i).pt()  // by default gives you pt with JEC applied
+                    << " " <<  hJets->at(i).eta() // 
+                    << std::endl;
+    }*/
+
+    // jets are pt ordered by default
+    // TODO: CHECKME - this was in 4_2, shouldnt change, but who knows...
+
+    int nJets =  hJets->size();
+    if (nJets>0){
+        m_floatBranches["leadJetPt"] = hJets->at(0).pt();
+        m_floatBranches["leadJetEta"] = hJets->at(0).eta();
+    }
+    if (nJets>1){
+        m_floatBranches["subleadJetPt"] = hJets->at(1).pt();
+        m_floatBranches["subleadJetEta"] = hJets->at(1).eta();
+    }
+
+    
 
 
 
