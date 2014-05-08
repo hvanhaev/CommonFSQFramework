efbff371ff5c7a86f84fa9ffe6deb3baef48665b
Tomasz <fruboes@cern.ch>
2014-05-08 09:51:22 +0200

    Go through trigger matches provided by PAT

--- plugins/MNTriggerAna.cc
+++ plugins/MNTriggerAna.cc
@@ -12,10 +12,12 @@
 */
 //
 // Original Author:  Tomasz Fruboes
-//         Created:  Fri, 11 Apr 2014 08:34:20 GMT
+//         Created:  Thu, 17 Apr 2014 16:06:29 GMT
 // $Id$
 //
 //
+
+
 
 
 // system include files
@@ -29,6 +31,14 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "TTree.h"
+
+#include "DataFormats/PatCandidates/interface/Jet.h"
+#include <DataFormats/PatCandidates/interface/TriggerEvent.h>
+
 //
 // class declaration
 //
@@ -42,9 +52,16 @@ class MNTriggerAna : public edm::EDAnalyzer {
 
 
    private:
+      void resetTrees();
       virtual void beginJob() override;
       virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
       virtual void endJob() override;
+
+      TTree *m_tree;
+      std::map<std::string, int> m_integerBranches;
+      std::map<std::string, float> m_floatBranches;
+      std::map<std::string, std::vector<reco::Candidate::LorentzVector> > m_vectorBranches;
+
 
       //virtual void beginRun(edm::Run const&, edm::EventSetup const&) override;
       //virtual void endRun(edm::Run const&, edm::EventSetup const&) override;
@@ -68,10 +85,99 @@ class MNTriggerAna : public edm::EDAnalyzer {
 MNTriggerAna::MNTriggerAna(const edm::ParameterSet& iConfig)
 
 {
-   //now do what ever initialization is needed
+
+    edm::Service<TFileService> tFileService;
+    m_tree = tFileService->make<TTree>("data", "data");
+
+    // define your branches. Registration will be made automagically
+    //
+    //  note: verify, that your data is filled in the tree. If you will
+    //        define your variable name as m_floatBranches["leadJetPt"]
+    //        and than missspell the name in the analyze method:
+    //          m_floatBranches["leadJetPT"] = 123; // note the wrong case in PT/Pt
+    //        program will continue to run not filling your variable
+    //
+    // use m_integerBranches for integer values
+    m_integerBranches["run"] = 0;
+    m_integerBranches["lumi"] = 0;
+    m_integerBranches["event"] = 0;
+
+    // use m_floatBranches for float values
+    m_floatBranches["leadJetPt"] = 0;
+    m_floatBranches["leadJetEta"] = 0;
+    m_floatBranches["subleadJetPt"] = 0;
+    m_floatBranches["subleadJetEta"] = 0;
+
+
+    // 
+    m_vectorBranches["pfJets"] = std::vector<reco::Candidate::LorentzVector>();
+
+    // integer branches auto registration
+    {
+        std::map<std::string, int>::iterator it =  m_integerBranches.begin();
+        std::map<std::string, int>::iterator itE =  m_integerBranches.end();
+        for (;it != itE;++it){
+            m_tree->Branch(it->first.c_str(), &it->second, (it->first+"/I").c_str());
+        }
+    }
+
+    // float branches auto registration
+    {
+        std::map<std::string, float>::iterator it =  m_floatBranches.begin();
+        std::map<std::string, float>::iterator itE =  m_floatBranches.end();
+        for (;it != itE;++it){
+            m_tree->Branch(it->first.c_str(), &it->second, (it->first+"/F").c_str());
+        }
+
+    }
+
+
+    // vector branches autoreg
+    {   
+        std::map<std::string, std::vector<reco::Candidate::LorentzVector> >::iterator it =  m_vectorBranches.begin();
+        std::map<std::string, std::vector<reco::Candidate::LorentzVector> >::iterator itE =  m_vectorBranches.end();
+        for (;it != itE;++it){
+            m_tree->Branch(it->first.c_str(), &it->second);//#;, (it->first+"/I").c_str());
+        }
+    }
+
+
 
 }
+void MNTriggerAna::resetTrees(){
 
+    // int branches
+    {
+        std::map<std::string, int>::iterator it =  m_integerBranches.begin();
+        std::map<std::string, int>::iterator itE =  m_integerBranches.end();
+        for (;it != itE;++it){
+                m_integerBranches[it->first]=0;
+        }
+    }
+
+    // float branches
+    {
+        std::map<std::string, float>::iterator it =  m_floatBranches.begin();
+        std::map<std::string, float>::iterator itE =  m_floatBranches.end();
+        for (;it != itE;++it){
+                m_floatBranches[it->first]=0;
+        }
+    }
+
+
+    //
+    {
+        std::map<std::string, std::vector<reco::Candidate::LorentzVector> >::iterator it =  m_vectorBranches.begin();
+        std::map<std::string, std::vector<reco::Candidate::LorentzVector> >::iterator itE =  m_vectorBranches.end();
+        for (;it != itE;++it){
+            m_vectorBranches[it->first].clear();
+        }
+    }
+
+
+
+
+}
 
 MNTriggerAna::~MNTriggerAna()
 {
@@ -90,8 +196,62 @@ MNTriggerAna::~MNTriggerAna()
 void
 MNTriggerAna::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-   using namespace edm;
-    std::cout << "miau" << std::endl;
+    resetTrees();
+    using namespace edm;
+
+    //std::cout << "Moin Agatko!" << std::endl;
+    m_integerBranches["run"] = iEvent.eventAuxiliary().run();
+    m_integerBranches["lumi"] = iEvent.eventAuxiliary().luminosityBlock();
+    m_integerBranches["event"] = iEvent.eventAuxiliary().event();
+
+    edm::Handle<edm::View<pat::Jet> > hJets;
+    //iEvent.getByLabel(edm::InputTag("patJets"), hJets);  // TODO/Fixme - inputTag from python cfg
+    iEvent.getByLabel(edm::InputTag("selectedPatJets"), hJets);  // TODO/Fixme - inputTag from python cfg
+
+    edm::Handle< pat::TriggerEvent > hTrEvent;
+    iEvent.getByLabel(edm::InputTag("patTriggerEvent"), hTrEvent);
+    const pat::TriggerObjectMatch * trMatches = hTrEvent->triggerObjectMatchResult("triggerMatchPF"); 
+
+
+    for (unsigned int i = 0; i<hJets->size(); ++i){
+        m_vectorBranches["pfJets"].push_back(hJets->at(i).p4());
+        edm::RefToBase<pat::Jet> jetRef = hJets->refAt(i);
+        edm::Ref<pat::TriggerObjectCollection > hltRefVec = (*trMatches)[jetRef];
+
+        reco::Candidate::LorentzVector hltp4;
+        if (hltRefVec.isNonnull()){
+            hltp4 = hltRefVec->p4();
+
+            std::cout << "Reference came from:" << hltRefVec->collection() << std::endl;
+        }
+
+        std::cout << "J: " <<  hJets->at(i).pt() << " " << hltp4.pt() << std::endl;
+
+
+
+        /*
+        std::cout   << "Jet:"  
+                    << " " <<  hJets->at(i).pt()  // by default gives you pt with JEC applied
+                    << " " <<  hJets->at(i).eta() // 
+                    << std::endl;
+        // */
+    }
+
+    // jets are pt ordered by default
+    // TODO: CHECKME - this was in 4_2, shouldnt change, but who knows...
+
+    int nJets =  hJets->size();
+    if (nJets>0){
+        m_floatBranches["leadJetPt"] = hJets->at(0).pt();
+        m_floatBranches["leadJetEta"] = hJets->at(0).eta();
+    }
+    if (nJets>1){
+        m_floatBranches["subleadJetPt"] = hJets->at(1).pt();
+        m_floatBranches["subleadJetEta"] = hJets->at(1).eta();
+    }
+
+    
+
 
 
 
@@ -104,6 +264,9 @@ MNTriggerAna::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    ESHandle<SetupData> pSetup;
    iSetup.get<SetupRecord>().get(pSetup);
 #endif
+
+    m_tree->Fill();
+
 }
 
 
