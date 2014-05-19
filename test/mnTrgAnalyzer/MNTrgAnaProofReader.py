3bc8f5e4bda491a1b2cbb83c6b8656a407b0244a
Tomasz <fruboes@cern.ch>
2014-05-19 16:34:10 +0200

    Split triggers, cache results

--- test/mnTrgAnalyzer/MNTrgAnaProofReader.py
+++ test/mnTrgAnalyzer/MNTrgAnaProofReader.py
@@ -21,6 +21,9 @@ class MNTrgAnaProofReader(ExampleProofReader):
     #def SlaveBegin( self, tree ):
     def configureAnalyzer(self):
         print 'configure: MNTrgAnaProofReader'
+        self.lastEv  = 0
+        self.lastRun = 0
+
         self.hist = {}
 
         self.hist["signalEffVsHLTThreshold_NOM"] = ROOT.TH1F("signalEffVsHLTThreshold_NOM",   "signalEffVsHLTThreshold_NOM",  50, -0.5, 49.5)
@@ -33,6 +36,50 @@ class MNTrgAnaProofReader(ExampleProofReader):
             self.GetOutputList().Add(self.hist[h])
 
         sys.stdout.flush()
+
+    def getTriggers(self):
+        newEvent = False
+        event = self.fChain.event
+        run = self.fChain.run
+        if event != self.lastEv:
+            newEvent = True
+        elif run != self.lastRun:
+            newEvent = True
+
+        if newEvent: # cache best trigger objects
+            self.lastRun =  run
+            self.lastEv = event
+
+            self.allL1 = [] # Currently all L1 jets not depending on eta
+            self.allHLT = [] # Currently all HLT jets not depending on eta
+            self.bestCAny = [] # CC or CF (or CB :)
+            self.bestFF = [] # can be FF or FB (or BB :)
+
+            todo = {}
+            todo["l1"] = self.fChain.l1Jets
+            todo["hlt"] =  getattr(self.fChain, self.hltCollection)
+            for t in todo:
+                curFwd = []
+                curCen = []
+                for jet in todo[t]:
+                    isCentral = abs(jet.eta()) < 3.0
+                    if isCentral:
+                        curCen.append(jet.pt())
+                    else:
+                        curFwd.append(jet.pt())
+
+                if t == "l1":
+                    self.allL1 = sorted(curFwd + curCen, reverse = True)
+                else:
+                    curCen = sorted(curCen, reverse = True)
+                    curFwd = sorted(curFwd, reverse = True)
+                    all = sorted(curCen + curFwd, reverse = True)
+                    self.allHLT = all
+                    self.bestCAny = sorted([all[0], all[1]], reverse = True)
+                    if len(curFwd) > 1:
+                        self.bestFF = sorted([curFwd[0], curFwd[1]], reverse = True)
+                            
+            
 
     def analyze(self):
         #event = self.fChain.event
@@ -70,23 +117,18 @@ class MNTrgAnaProofReader(ExampleProofReader):
         # and find two with the highest PT
         # TODO  : recoJet2HLTjet matching
         # TODO2 : hltJet2l1Jet matching
-        HLTpts = []
 
-        if level == 2:
-            hltJets = getattr(self.fChain, self.hltCollection)
-        elif level == 1:
-            hltJets = self.fChain.l1Jets
-        else:
+        if level != 2 and level != 1:
             raise Exception("level should be equal to 1 or 2")
 
-
-        for i in xrange(hltJets.size()):
-            pt = hltJets.at(i).pt()
-            HLTpts.append(pt)
-
+        self.getTriggers() # cache best L1 and HLT objects
         lowestPTNeededForAcceptForThisEvent = 0 # if it stays 0 - less than two HLT jets present in the event
-        if len(HLTpts)>= minObjects:
-            lowestPTNeededForAcceptForThisEvent = sorted(HLTpts, reverse=True)[minObjects-1]
+        if level == 1:
+            if len(self.allL1)>= minObjects:
+                lowestPTNeededForAcceptForThisEvent = sorted(self.allL1, reverse=True)[minObjects-1]
+        elif level == 2:
+            if len(self.allHLT)>= minObjects:
+                lowestPTNeededForAcceptForThisEvent = sorted(self.allHLT, reverse=True)[minObjects-1]
 
 
         # We found two HLT jets with pt at least equall to lowestPTNeededForAcceptForThisEvent
