528748b6f6815f8c47fa39c69ca379d50687c920
Tomasz <fruboes@cern.ch>
2014-05-22 13:44:12 +0200

    inici

---
+++ scripts/copyAnaDataLocal.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import os, sys, imp
+import ROOT
+ROOT.gROOT.SetBatch(True)
+from ROOT import *
+
+from MNTriggerStudies.MNTriggerAna.GetDatasetInfo import getTreeFilesAndNormalizations
+import MNTriggerStudies.MNTriggerAna.Util
+
+from optparse import OptionParser
+
+if __name__ == "__main__":
+    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+    ROOT.gSystem.Load("libFWCoreFWLite.so")
+    AutoLibraryLoader.enable()
+
+
+    parser = OptionParser()
+    (options, args) = parser.parse_args()
+    if len(args) != 1:
+        print "Usage: copyAnaDataLocal.py localDir"
+        print "    usefull for making local copy of data (e.g. nfs to /tmp/)"
+        sys.exit(0)
+
+    target = args[0]
+
+
+    treeFilesAndNormalizations = getTreeFilesAndNormalizations(maxFiles = None)
+
+    anaDefFile = os.environ["SmallXAnaDefFile"]
+    mod_dir, filename = os.path.split(anaDefFile)
+    mod, ext = os.path.splitext(filename)
+    f, filename, desc = imp.find_module(mod, [mod_dir])
+    mod = imp.load_module(mod, f, filename, desc)
+
+
+
+    localBasePathPAT = mod.PATbasePATH
+    localBasePathTrees = mod.TTreeBasePATH
+
+    targetPAT = target + "/PAT/"
+    targetTrees = target + "/trees/"
+
+    print "Copy to:"
+    print targetPAT
+    print targetTrees
+
+    for s in treeFilesAndNormalizations:
+        for f in treeFilesAndNormalizations[s]["files"]:
+            src = f
+
+            if localBasePathPAT in f:
+                trg = f.replace(localBasePathPAT, targetPAT)
+            elif localBasePathTrees in f:
+                trg = f.replace(localBasePathTrees, targetTrees)
+            else:
+                print "File not known: ", f # shouldnt happen
+                continue
+
+            if os.path.isfile(trg): 
+                continue
+            dirname = os.path.dirname(trg)
+            os.system("mkdir -p "+  dirname)
+            #print src, trg
+            os.system("cp " + src + " " + trg)
+
+
+    print 'PATbasePATH="'+targetPAT+'"'
+    print 'TTreeBasePATH="' + targetTrees + '"'
+
+
