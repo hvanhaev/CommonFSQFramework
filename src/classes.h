c62d0127dce2a527f71116de3bb3648679857983
Tomasz <fruboes@cern.ch>
2014-05-23 16:00:50 +0200

    include lumiReweighting class

--- src/classes.h
+++ src/classes.h
@@ -1,8 +1,12 @@
 #include "FWCore/ParameterSet/interface/FileInPath.h"
+#include "PhysicsTools/Utilities/interface/LumiReWeighting.h"
+
 
 namespace {
   struct dictionary {
     edm::FileInPath v1;
+    edm::LumiReWeighting v2;
+
 
   };
 }
