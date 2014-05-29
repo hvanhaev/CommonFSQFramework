cf52c32798d8f6dca2ec2d4f1eb1e8bb62889f89
Tomasz <fruboes@cern.ch>
2014-05-29 13:49:51 +0200

    put fitting part into separate function

--- test/MNxsectionAna/balanceFitAndPlot.py
+++ test/MNxsectionAna/balanceFitAndPlot.py
@@ -12,6 +12,8 @@ from array import array
 import resource
 import time
 
+import Queue
+import threading
 
 def getUncertaintyBand(histos, hCentral):
     if len(histos) == 0:
@@ -54,6 +56,73 @@ def getUncertaintyBand(histos, hCentral):
     #    graphBand.Draw("3") 
 
     return ret
+
+
+def fit(ds, q):
+#def fit(inputMap, q):
+
+    canvas = ROOT.TCanvas()
+    dsReduced = inputMap["dsReduced"]
+    myVar = inputMap["myVar"]
+
+    #myVar = vars[t][vary("balance")]
+    meanVal = dsReduced.mean(myVar)
+    sigma   = dsReduced.sigma(myVar)
+    rangeLow = meanVal - sigma*0.75
+    rangeHigh = meanVal + sigma*0.75
+
+    mean2 = RooRealVar("mean","mean of gaussian", 0, -1.5, 1.5)
+    sigma2 = RooRealVar("sigma","width of gaussian", .1, 0, 1)
+    gauss2 = RooGaussian("gauss","gaussian PDF",myVar, mean2, sigma2)
+    gauss2.fitTo(dsReduced, ROOT.RooFit.Range(rangeLow, rangeHigh), ROOT.RooFit.PrintLevel(-1)) # this exludes -1 point ("no jet matched point")
+
+    balanceVariable = "diJet balance"
+    frame = myVar.frame(ROOT.RooFit.Range(-1.5,1))
+    frame.GetXaxis().SetTitle(balanceVariable)
+    #sampleList[s]["RooDS"].plotOn(frame)
+    dsReduced.plotOn(frame)
+    gauss2.plotOn(frame)
+
+    gauss2.paramOn(frame, ROOT.RooFit.Layout(0.2, 0.5,0.95)) # , RooFit.Label("Gauss Fit"))
+
+    # myVar = treeReader.variables[balanceVariable]["RooVar"]
+    ptProbeJetVar = inputMap["ptProbeJetVar"]
+    meanPT = dsReduced.mean(ptProbeJetVar)
+    sigmaPT = dsReduced.sigma(ptProbeJetVar)
+
+    etaMin = inputMap["etaMin"]
+    etaMax = inputMap["etaMax"]
+    minPtAVG = inputMap["minPtAVG"] 
+
+    box = ROOT.TPaveText(0.2,0.45, 0.50, 0.8, "BRNDC")
+    box.SetFillColor(0)
+    #box.AddText(tag)
+    box.AddText("avg(p^{probe}_{T})=%10.2f" % meanPT)
+    box.AddText("\sigma(p^{probe}_{T})=%10.2f" % sigmaPT )
+    box.AddText(str(etaMin) + " < |#eta_{probe}| < "+str(etaMax))
+    box.AddText("p_{T}^{ave} > "+str(minPtAVG))
+    #box.AddText("probe jet p_{T} > "+str(minPtAVG))
+
+    frame.addObject(box)
+    frame.Draw()
+    odir = "~/tmp/balance/"
+    preName = odir + myVar.GetName() + "_ptAveMin_" + str(minPtAVG)  \
+    + "_etaMin_" + str(etaMin).replace(".", "_") \
+    + "_etaMax_" + str(etaMax).replace(".", "_") 
+    #+ "_" + tag
+    fname = preName + ".png"
+    canvas.Print(fname)
+
+    fitResult = {}
+    fitResult["iEta"] = inputMap["iEta"]
+    fitResult["mean"]      = mean2.getVal()
+    fitResult["meanErr"] = mean2.getError()
+    fitResult["gaussWidth"] = sigma2.getVal()
+    fitResult["gaussWidthErr"] = sigma2.getError()
+    #q.put(fitResult)
+
+    #return fitResult
+
 
 
 
@@ -122,6 +191,8 @@ def main():
     ds = {}
 
     variations = set()
+
+
     for t in trees:
         print "RooDataset:",t
         vars[t] = {}
@@ -159,7 +230,6 @@ def main():
     etaRanges = []
     etaRanges.extend([1.401, 1.701, 2.001, 2.322, 2.411, 2.601, 2.801, 3.001, 3.201, 3.501, 3.801, 4.101, 4.701])
     minPtAVG = 45
-    canvas = ROOT.TCanvas()
 
 
     curPath = ROOT.gDirectory.GetPath()
@@ -170,6 +240,7 @@ def main():
     ROOT.gDirectory.cd(curPath)
 
 
+    queue = Queue.Queue()
     for t in ds:
         for v in variations:
             if t=="data" and v != "central":
@@ -195,58 +266,22 @@ def main():
                 print "Reduce"
                 dsReduced = ds[t].reduce(cut)
                 print "Reduce...done"
+                inputMap = {}
+                inputMap["dsReduced"] = dsReduced
+                inputMap["myVar"] = vars[t][vary("balance")]
+                inputMap["ptProbeJetVar"] = vars[t][vary("probePt")]
+                inputMap["etaMin"] = etaMin
+                inputMap["etaMax"] = etaMax
+                inputMap["minPtAVG"] = minPtAVG
+                inputMap["iEta"] = iEta # xcheck only
 
-                myVar = vars[t][vary("balance")]
-                meanVal = dsReduced.mean(myVar)
-                sigma   = dsReduced.sigma(myVar)
-                rangeLow = meanVal - sigma*0.75
-                rangeHigh = meanVal + sigma*0.75
-
-                mean2 = RooRealVar("mean","mean of gaussian", 0, -1.5, 1.5)
-                sigma2 = RooRealVar("sigma","width of gaussian", .1, 0, 1)
-                gauss2 = RooGaussian("gauss","gaussian PDF",myVar, mean2, sigma2)
-                gauss2.fitTo(dsReduced, ROOT.RooFit.Range(rangeLow, rangeHigh), ROOT.RooFit.PrintLevel(-1)) # this exludes -1 point ("no jet matched point")
-
-                balanceVariable = "diJet balance"
-                frame = myVar.frame(ROOT.RooFit.Range(-1.5,1))
-                frame.GetXaxis().SetTitle(balanceVariable)
-                #sampleList[s]["RooDS"].plotOn(frame)
-                dsReduced.plotOn(frame)
-                gauss2.plotOn(frame)
-
-                gauss2.paramOn(frame, ROOT.RooFit.Layout(0.2, 0.5,0.95)) # , RooFit.Label("Gauss Fit"))
-
-                # myVar = treeReader.variables[balanceVariable]["RooVar"]
-                ptProbeJetVar = vars[t][vary("probePt")]
-                meanPT = dsReduced.mean(ptProbeJetVar)
-                sigmaPT = dsReduced.sigma(ptProbeJetVar)
-
-                box = ROOT.TPaveText(0.2,0.45, 0.50, 0.8, "BRNDC")
-                box.SetFillColor(0)
-                #box.AddText(tag)
-                box.AddText("avg(p^{probe}_{T})=%10.2f" % meanPT)
-                box.AddText("\sigma(p^{probe}_{T})=%10.2f" % sigmaPT )
-                box.AddText(str(etaMin) + " < |#eta_{probe}| < "+str(etaMax))
-                box.AddText("p_{T}^{ave} > "+str(minPtAVG))
-                #box.AddText("probe jet p_{T} > "+str(minPtAVG))
-
-                frame.addObject(box)
-                frame.Draw()
-                odir = "~/tmp/balance/"
-                preName = odir + v + "_ptAveMin_" + str(minPtAVG)  \
-                + "_etaMin_" + str(etaMin).replace(".", "_") \
-                + "_etaMax_" + str(etaMax).replace(".", "_") 
-                #+ "_" + tag
-                fname = preName + ".png"
-                canvas.Print(fname)
-
-                fitResult = {}
-                fitResult["iEta"] = iEta
-                fitResult["mean"]      = mean2.getVal()
-                fitResult["meanErr"] = mean2.getError()
-                fitResult["gaussWidth"] = sigma2.getVal()
-                fitResult["gaussWidthErr"] = sigma2.getError()
+                #ptProbeJetVar
+                fitResult = fit(inputMap)
                 results.append(fitResult)
+
+                #t = threading.Thread(target=fit, args = (dsReduced, queue))
+                #t.daemon = False
+                #t.start()
 
 
             # all etas done. Create summary (vs eta) histogram
