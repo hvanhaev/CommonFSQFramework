459f4e37266eefa5e58e1e8310f9049ca181457a
Tomasz <fruboes@cern.ch>
2014-05-29 09:51:44 +0200

    Fits are working

--- test/MNxsectionAna/balanceFitAndPlot.py
+++ test/MNxsectionAna/balanceFitAndPlot.py
@@ -116,7 +116,7 @@ def main():
             print "data tree after merge: ", trees[t].GetEntries()
 
 
-    vars = [] # note: we whave to save the variables outside the loop, otherwise they get
+    vars = {} # note: we whave to save the variables outside the loop, otherwise they get
               #       garbage collected by python leading to a crash
 
     ds = {}
@@ -124,6 +124,7 @@ def main():
     variations = set()
     for t in trees:
         print "RooDataset:",t
+        vars[t] = {}
         tree = trees[t]
         observables = ROOT.RooArgSet()
         print "  min/max"
@@ -139,7 +140,7 @@ def main():
             rmax = rmax+abs(rmin/100.)
             #print name, rmin, rmax
             roovar = ROOT.RooRealVar( name, name, rmin, rmax, "")
-            vars.append(roovar)
+            vars[t][name] = roovar
             print "Creating variable", name, type(roovar)
             sys.stdout.flush()
             observables.add(roovar)
@@ -158,6 +159,7 @@ def main():
     etaRanges = []
     etaRanges.extend([1.401, 1.701, 2.001, 2.322, 2.411, 2.601, 2.801, 3.001, 3.201, 3.501, 3.801, 4.101, 4.701])
     minPtAVG = 45
+    canvas = ROOT.TCanvas()
     for t in ds:
         for v in variations:
             if t=="data" and v != "central":
@@ -178,10 +180,51 @@ def main():
                 print cut
 
                 dsReduced = ds[t].reduce(cut)
-                #mean2 = RooRealVar("mean","mean of gaussian", 0, -1.5, 1.5)
-                #sigma2 = RooRealVar("sigma","width of gaussian", .1, 0, 1)
-                #gauss2 = RooGaussian("gauss","gaussian PDF",myVar, mean2, sigma2)
-                #gauss2.fitTo(dsReduced, RooFit.Range(rangeLow, rangeHigh), RooFit.PrintLevel(-1)) # this exludes -1 point ("no jet matched point")
+                myVar = vars[t][vary("balance")]
+                meanVal = dsReduced.mean(myVar)
+                sigma   = dsReduced.sigma(myVar)
+                rangeLow = meanVal - sigma*0.75
+                rangeHigh = meanVal + sigma*0.75
+
+                mean2 = RooRealVar("mean","mean of gaussian", 0, -1.5, 1.5)
+                sigma2 = RooRealVar("sigma","width of gaussian", .1, 0, 1)
+                gauss2 = RooGaussian("gauss","gaussian PDF",myVar, mean2, sigma2)
+                gauss2.fitTo(dsReduced, ROOT.RooFit.Range(rangeLow, rangeHigh), ROOT.RooFit.PrintLevel(-1)) # this exludes -1 point ("no jet matched point")
+
+                balanceVariable = "diJet balance"
+                frame = myVar.frame(ROOT.RooFit.Range(-1.5,1))
+                frame.GetXaxis().SetTitle(balanceVariable)
+                #sampleList[s]["RooDS"].plotOn(frame)
+                dsReduced.plotOn(frame)
+                gauss2.plotOn(frame)
+
+                gauss2.paramOn(frame, ROOT.RooFit.Layout(0.2, 0.5,0.95)) # , RooFit.Label("Gauss Fit"))
+
+                # myVar = treeReader.variables[balanceVariable]["RooVar"]
+                ptProbeJetVar = vars[t][vary("probePt")]
+                meanPT = dsReduced.mean(ptProbeJetVar)
+                sigmaPT = dsReduced.sigma(ptProbeJetVar)
+
+                box = ROOT.TPaveText(0.2,0.45, 0.50, 0.8, "BRNDC")
+                box.SetFillColor(0)
+                #box.AddText(tag)
+                box.AddText("avg(p^{probe}_{T})=%10.2f" % meanPT)
+                box.AddText("\sigma(p^{probe}_{T})=%10.2f" % sigmaPT )
+                box.AddText(str(etaMin) + " < |#eta_{probe}| < "+str(etaMax))
+                box.AddText("p_{T}^{ave} > "+str(minPtAVG))
+                #box.AddText("probe jet p_{T} > "+str(minPtAVG))
+
+                frame.addObject(box)
+                frame.Draw()
+                odir = "~/tmp/balance/"
+                preName = odir + v + "_ptAveMin_" + str(minPtAVG)  \
+                + "_etaMin_" + str(etaMin).replace(".", "_") \
+                + "_etaMax_" + str(etaMax).replace(".", "_") 
+                #+ "_" + tag
+                fname = preName + ".png"
+                canvas.Print(fname)
+
+
 
 
 
