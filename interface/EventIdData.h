04916a6043223c199c8f7bcb1b160b3ce7513a73
Tomasz <fruboes@cern.ch>
2014-07-03 16:30:17 +0200

    inici

---
+++ interface/EventIdData.h
@@ -0,0 +1,13 @@
+#ifndef EventIdData_h
+#define EventIdData_h
+
+#include "MNTriggerStudies/MNTriggerAna/interface/EventViewBase.h"
+
+class EventIdData: public EventViewBase{
+   public:
+      EventIdData(const edm::ParameterSet&, TTree * tree);
+      virtual void fill(const edm::Event&, const edm::EventSetup&);
+
+
+};
+#endif
---
+++ src/EventIdData.cc
@@ -0,0 +1,13 @@
+#include "MNTriggerStudies/MNTriggerAna/interface/EventIdData.h"
+
+EventIdData::EventIdData(const edm::ParameterSet& iConfig, TTree * tree){
+
+
+
+}
+
+
+void EventIdData::fill(const edm::Event&, const edm::EventSetup&){
+
+
+}
