ad2965bb6b847bbb05cb50e6696602720768f76e
Tomasz <fruboes@cern.ch>
2014-08-29 09:16:42 +0200

    more variables for trg studies

--- test/mnTrgAnalyzer/HLTBalanceTreeProducer.py
+++ test/mnTrgAnalyzer/HLTBalanceTreeProducer.py
@@ -28,6 +28,13 @@ class HLTBalanceTreeProducer(BalanceTreeProducer.BalanceTreeProducer):
         self.addExternalVar(["hltPtAve"])
         self.addExternalVar(["hltPtCen"])
         self.addExternalVar(["hltPtFwd"])
+        self.addExternalVar(["hltL1MatchPtCen"])
+        self.addExternalVar(["hltL1MatchPtFwd"])
+        self.addExternalVar(["l1DoubleJet"])
+        self.addExternalVar(["l1SingleJetCentral"])
+        self.addExternalVar(["l1SingleJetForward"])
+
+
         self.addExternalVar(["PUNumInteractions"])
         self.addExternalVar(["puTrueNumInteractions"])
 
@@ -54,12 +61,7 @@ class HLTBalanceTreeProducer(BalanceTreeProducer.BalanceTreeProducer):
         #self.balanceProd =  BalanceTreeProducer()
         #self.GetOutputList().Add(self.tree)
 
-    def analyze(self):
-        #print "XAXA" ,  self.HLTptAve
-        # hltAK5PFJetL1FastL2L3Corrected
-        #setattr(self.fChain, "ngoodVTX", 2) # TODO: fChain proxy ?!?
-
-        hltJets = self.fChain.hltAK4PFJetsCorrected
+    def getHLTTagProbe(self, hltJets):
         probe = None
         tag = None
 
@@ -73,20 +75,68 @@ class HLTBalanceTreeProducer(BalanceTreeProducer.BalanceTreeProducer):
             if probeCand and (probe == None or probe.pt() < pt): probe = j
             if tagCand and (tag == None or tag.pt() < pt): tag = j
 
+        return (tag, probe)
+
+    def analyze(self):
+        tag, probe = self.getHLTTagProbe(self.fChain.hltAK4PFJetsCorrected)
+        tagL1match, probeL1match = self.getHLTTagProbe(self.fChain.hltPFJetsCorrectedMatchedToL1)
+
+
         avePT = 0 # this are all HLT level values!
         tagPT = 0 
         probePT = 0
+        tagL1matchPT = 0 
+        probeL1matchPT = 0
         
-        if probe != None and tag!=None  and probe != tag:
+        if probe != None and tag!=None:  
+            #print "XXX", tag.pt(), probe.pt()
             avePT = (tag.pt() + probe.pt())/2.
             tagPT = tag.pt()
             probePT = probe.pt()
+
+        if probeL1match != None:
+            probeL1matchPT = probeL1match.pt()
+
+        if tagL1match != None:
+            tagL1matchPT = tagL1match.pt()
+
+
+        self.setExternalVar("hltL1MatchPtCen", tagL1matchPT)
+        self.setExternalVar("hltL1MatchPtFwd", probeL1matchPT)
+
 
         self.setExternalVar("hltPtAve", avePT)
         self.setExternalVar("hltPtCen", tagPT)
         self.setExternalVar("hltPtFwd", probePT)
         self.setExternalVar("PUNumInteractions", self.fChain.PUNumInteractions)
         self.setExternalVar("puTrueNumInteractions", self.fChain.puTrueNumInteractions)
+
+        l1Jets = self.fChain.l1Jets
+        #print "XX"
+        pts = []
+        hardestL1Central = 0
+        hardestL1Forwad  = 0
+
+        # eta l1 scale:  -0.174 -0.5215 -0.8695 -1.218 -1.566 -1.956 -2.586 -3.25 -3.75 -4.25 -4.75
+        for j in l1Jets:
+            pts.append(j.pt())
+            if abs(j.eta()) < 1.7 and hardestL1Central < j.pt():
+                hardestL1Central = j.pt()
+            if abs(j.eta()) > 2.5 and hardestL1Forwad < j.pt():
+                hardestL1Forwad = j.pt()
+
+        top2 = sorted(pts)[-2:]
+        doubleJetLowestThr = 1000
+        if len(top2) == 2:
+            doubleJetLowestThr = top2[0]
+
+        #print "A", pts
+        #print "B", top2
+        #print "C", doubleJetLowestThr
+        self.setExternalVar("l1DoubleJet", doubleJetLowestThr)
+        self.setExternalVar("l1SingleJetCentral", hardestL1Central)
+        self.setExternalVar("l1SingleJetForward", hardestL1Forwad)
+
         pu = self.fChain.PUNumInteractions
         genW = BalanceTreeProducer.BalanceTreeProducer.genWeight(self)
         for l in self.newlumiWeighters:
@@ -113,7 +163,6 @@ if __name__ == "__main__":
     nWorkers = None # Use all
 
     slaveParams = {}
-    slaveParams["threshold"] = 35.
 
     #slaveParams["recoJetCollection"] = "pfJets"
     #slaveParams["recoJetCollection"] = "pfJetsSmear"
