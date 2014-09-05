f90743af21725e37c31df8afc4ef2b89521b9962
Tomasz <fruboes@cern.ch>
2014-09-05 14:25:27 +0200

    Add placeholder for trigger and variation

--- test/CSA14/CSA14_dndeta.py
+++ test/CSA14/CSA14_dndeta.py
@@ -2,20 +2,27 @@
 import MNTriggerStudies.MNTriggerAna.ExampleProofReader
 from  MNTriggerStudies.MNTriggerAna.RecoTracksGetter import RecoTracksGetter
 
-
 import sys, os, time
 sys.path.append(os.path.dirname(__file__))
 
 import ROOT
 ROOT.gROOT.SetBatch(True)
 
-#from array import *
-
 class CSA14_dndeta(MNTriggerStudies.MNTriggerAna.ExampleProofReader.ExampleProofReader):
     def init( self):
+        #self.triggers   = ["minbias", "zerobias"]
+        self.triggers   = ["minbias", ]
+        self.variations = ["central"] # only a central value now
+        
         self.hist = {}
-        self.hist["pt"] =  ROOT.TH1F("pt", "pt;p_T [GeV]",  100, -2.6, 2.6)
-        self.hist["eta"] =  ROOT.TH1F("eta",   "eta; #eta",  100, -2.6, 2.6)
+        for t in self.triggers:
+            for v in self.variations:
+                histPostfix = "_" + v + "_" + t
+                ptName = "pt"+histPostfix
+                self.hist[ptName] =  ROOT.TH1F(ptName, ptName+";p_T [GeV]",  100,  0, 10)
+                etaName = "eta"+histPostfix
+                self.hist[etaName] =  ROOT.TH1F(etaName, etaName+"; #eta",  100, -2.7, 2.7)
+
         for h in self.hist:
             self.hist[h].Sumw2()
             self.GetOutputList().Add(self.hist[h])
@@ -29,17 +36,31 @@ class CSA14_dndeta(MNTriggerStudies.MNTriggerAna.ExampleProofReader.ExampleProof
         # direct access to branch to compare efficiency as when getter is used:
         #for i in xrange(self.fChain.recoTracksrecoTracks.size()):
         #    trackp4 = self.fChain.recoTracksrecoTracks.at(i)
-        self.tracks.newEvent(self.fChain)
-        for track in self.tracks.get():
-            trackp4 = track.recoTracks # note : in next skim this will become: trackp4 = track.p4
-            eta = trackp4.eta()
-            pt =    trackp4.pt()
-            self.hist["pt"].Fill(pt, weight) # always use weight when filling
-            self.hist["eta"].Fill(eta, weight) # always use weight when filling
-            
+        self.tracks.newEvent(self.fChain) # 
 
+        for trg in self.triggers: # iterate over all triggers we use
+            trigger = False
+            if trg == "minbias":
+                trigger = True  # TODO in future versions: actually check for trigger
+            elif trg == "zerobias":
+                trigger = True  # TODO: as above
 
-        return 1
+            if not trigger: continue
+
+            # iterate over all variations (again - placs is a placeeholder for future)
+            #  eg.  if we would use jets our variations would look like
+            #   ["central", "jecUp", "jecDown"]  
+            #   our JetGetter (not used here) is smart enough to give jets with momentum variaed for +1sigma
+            #     when calling self.jetGetter.get("jecUp") (and -1sigma for jecDown)
+            for variation in self.variations: 
+                histPostfix = "_" + variation + "_" + trg
+                for track in self.tracks.get(variation):
+                    trackp4 = track.recoTracks # wrong naming in Samples_CSA14_Tracks_20140904 skim. 
+                                               # this will evolve to trackp4 = track.p4 in next version 
+                    eta = trackp4.eta()
+                    pt =    trackp4.pt()
+                    self.hist["pt"+histPostfix].Fill(pt, weight) # always use weight when filling
+                    self.hist["eta"+histPostfix].Fill(eta, weight) # always use weight when filling
 
     # note: this is executed on the slave (ie output will appear in logs),
     #       - before merging the histograms. Here we should apply the histogram 
@@ -80,15 +101,25 @@ if __name__ == "__main__":
     nWorkers = None # Use all cores
 
     quickTest = False
-    quickTest = True
+    #quickTest = True
     
     if quickTest:
         # Run printTTree.py alone to get the samples list
-        sampleList = []
-        sampleList.append("data_MinBias_TuneCUETP8S1-HERAPDF_13TeV-pythia8")
+        #sampleList = []
+        #sampleList.append("data_MinBias_TuneCUETP8S1-HERAPDF_13TeV-pythia8")
         maxFilesMC = 1
         maxFilesData = 1
         nWorkers = 1
+
+    # another possibility to process bit faster: process only part of MC
+    maxFilesMC = 12
+
+    # only processin part of data will lead to wrong normalization of the data histograms
+    # - since lumi values are not automatically updated to reflect the fact that only
+    # part of sample was processed
+    #
+    # This is not a problem for MC, while here normalization factor is calculated using
+    # number of processed events
 
 
     slaveParams = {}
