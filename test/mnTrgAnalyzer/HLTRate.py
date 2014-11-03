d163694b6aeeecf66c36b958acd6a514f2e4af67
Tomasz <fruboes@cern.ch>
2014-11-03 11:16:22 +0100

    handle genWeights correctly

--- test/mnTrgAnalyzer/HLTRate.py
+++ test/mnTrgAnalyzer/HLTRate.py
@@ -21,19 +21,69 @@ import BaseTrigger
 
 class HLTRate(MNTriggerStudies.MNTriggerAna.ExampleProofReader.ExampleProofReader):
     def init(self):
+        self.avgW = 0.
+        self.cnt = 0
+
+        self.normFactor = self.getNormalizationFactor()
+        puFile = edm.FileInPath("MNTriggerStudies/MNTriggerAna/test/mnTrgAnalyzer/PUhists.root").fullPath()
+        self.newlumiWeighters = {}
+        self.newlumiWeighters["flat2050toPU20"] = edm.LumiReWeighting(puFile, puFile, "Flat20to50/pileup", "PU20/pileup")
+        #self.newlumiWeighters["flat010toPU10"] = edm.LumiReWeighting(puFile, puFile, "Flat0to10/pileup", "PU10/pileup")
+        #self.newlumiWeighters["flat010toPU1"] = edm.LumiReWeighting(puFile, puFile, "Flat0to10/pileup", "PU1/pileup")
+        #self.newlumiWeighters["PU20toPU20"] = edm.LumiReWeighting(puFile, puFile, "PU20/pileup", "PU20/pileup")
+
+
+
         getter = BaseTrigger.TriggerObjectsGetter(self.fChain, self.hltCollection)
-        self.fbTrigger = BaseTrigger.ForwardBackwardTrigger(getter)
-        self.doubleFwdTrigger = BaseTrigger.DoubldForwardTrigger(getter)
-        self.atLeastOneCentral = BaseTrigger.DoubleJetWithAtLeastOneCentralJetTrigger(getter)
-        self.ptAveForJecTrigger = BaseTrigger.PTAveForHFJecTrigger(getter)
-        self.singleJetTrigger = BaseTrigger.SingleJetTrigger(getter)
-                    
+        getterReco = BaseTrigger.TriggerObjectsGetter(self.fChain, self.recoCollection)
+        #self.fbTrigger = BaseTrigger.ForwardBackwardTrigger(getter)
+        #self.doubleFwdTrigger = BaseTrigger.DoubldForwardTrigger(getter)
+        #self.atLeastOneCentral = BaseTrigger.DoubleJetWithAtLeastOneCentralJetTrigger(getter)
+        #self.ptAveForJecTrigger = BaseTrigger.PTAveForHFJecTrigger(getter)
+        #self.singleJetTrigger = BaseTrigger.SingleJetTrigger(getter)
+        self.messed = BaseTrigger.PTAveMessedTrigger(getter)         
+        self.proper = BaseTrigger.PTAveProperTrigger(getter)
+        self.properForLumiEst = BaseTrigger.PTAveProperTrigger(getter,  etaTag = 1.3, etaProbeMin = 3.139, etaProbeMax = 5.191, minDphi=2.7)
+
+        self.properReco = BaseTrigger.PTAveProperTrigger(getterReco, etaTag=1.3, etaProbeMin=3.139)
+
+           
         self.histos = {} # name -> trigger, histo, rLow, rHigh
-        self.histos["fb"] = [self.fbTrigger, None, 14.5, 29.5]
-        self.histos["doubleForward"] = [self.doubleFwdTrigger, None, 14.5, 29.5]
-        self.histos["atLeastOneCentral"] = [self.atLeastOneCentral, None, 14.5, 29.5]
-        self.histos["singleJet"] = [self.singleJetTrigger, None, 299.5, 399.5]
-        self.histos["ptAveHFJEC"] = [self.ptAveForJecTrigger, None, 14.5, 39.5]
+        #self.histos["ptAveMessed"] = [self.messed, None, 59.5, 81.5]
+
+        '''
+        self.histos["ptAveProper30_60"] = [self.properForLumiEst, None, 29.5, 61.5]
+        self.histos["ptAveProper60_200"] = [self.properForLumiEst, None, 59.5, 201.5]
+        self.histos["ptAveProper60_80"] = [self.properForLumiEst, None, 59.5, 81.5]
+        self.histos["ptAveProper80_120"] = [self.properForLumiEst, None, 79.5, 121.5]
+        self.histos["ptAveProper120_200"] = [self.properForLumiEst, None, 119.5, 200.5]
+        self.histos["ptAveProper200_300"] = [self.properForLumiEst, None, 199.5, 300.5]
+        self.histos["ptAveProper300_400"] = [self.properForLumiEst, None, 299.5, 400.5]
+        '''
+
+        self.histos["ptAveProper30_60"] = [self.proper, None, 29.5, 61.5]
+        self.histos["ptAveProper60_200"] = [self.proper, None, 59.5, 201.5]
+        self.histos["ptAveProper60_80"] = [self.proper, None, 59.5, 81.5]
+        self.histos["ptAveProper80_120"] = [self.proper, None, 79.5, 121.5]
+        self.histos["ptAveProper120_200"] = [self.proper, None, 119.5, 200.5]
+        self.histos["ptAveProper200_300"] = [self.proper, None, 199.5, 300.5]
+        self.histos["ptAveProper300_400"] = [self.proper, None, 299.5, 400.5]
+
+
+
+
+        #self.histos["recoptAveProper30_60"] = [self.properReco, None, 29.5, 61.5]
+        #self.histos["recoptAveProper60_80"] = [self.properReco, None, 59.5, 81.5]
+        #self.histos["recoptAveProper80_120"] = [self.properReco, None, 79.5, 121.5]
+        #self.histos["recoptAveProper120_200"] = [self.properReco, None, 119.5, 200.5]
+        #self.histos["recoptAveProper200_300"] = [self.properReco, None, 199.5, 300.5]
+
+
+
+        #self.histos["doubleForward"] = [self.doubleFwdTrigger, None, 14.5, 29.5]
+        #self.histos["atLeastOneCentral"] = [self.atLeastOneCentral, None, 14.5, 29.5]
+        #self.histos["singleJet"] = [self.singleJetTrigger, None, 299.5, 399.5]
+        #self.histos["ptAveHFJEC"] = [self.ptAveForJecTrigger, None, 14.5, 39.5]
 
         for t in self.histos:
             nbins = int(self.histos[t][3]-self.histos[t][2])
@@ -42,35 +92,48 @@ class HLTRate(MNTriggerStudies.MNTriggerAna.ExampleProofReader.ExampleProofReade
             self.histos[t][1].Sumw2()
             self.GetOutputList().Add(self.histos[t][1])
 
-    # TODO: implement weighting!!!!
-    def fillRate(self, hist, maxThr):
+    def genWeight(self):
+        #print "ASDFASD", self.fChain.genWeight
+        return self.fChain.genWeight*self.normFactor
+
+    def fillRate(self, hist, maxThr, weight):
         nbins = hist.GetNbinsX()
         getBinCenter = hist.GetXaxis().GetBinCenter
-        fill = hist.Fill
         for i in xrange(1,nbins+1):
+            #binCenter = int(getBinCenter(i))
             binCenter = getBinCenter(i)
-            if binCenter <= maxThr:
-                fill(binCenter)
+
+            # As always "<=" is a tricky thing...
+            if binCenter < maxThr or abs(binCenter-maxThr) < 0.1:
+                hist.Fill(binCenter, weight)
             else:
                 break
-
-        del fill
         del getBinCenter
 
+
     def analyze(self):
-        weight = 1. # calculate your event weight here
+        weight = self.genWeight()  # calculate your event weight here
+        self.avgW += self.fChain.genWeight
+        self.cnt += 1
+        pu = self.fChain.PUNumInteractions
+        for l in self.newlumiWeighters:
+            wPU = self.newlumiWeighters[l].weight(pu)
+            break
+
         # TODO: implement weighting!!!!
         for t in self.histos:
             thr = self.histos[t][0].getMaxThreshold()
             if thr > 0.5:
-                self.fillRate(self.histos[t][1], thr )
+                self.fillRate(self.histos[t][1], thr, weight*wPU )
 
     def finalize(self):
-        print "Finalize:"
-        normFactor = self.getNormalizationFactor()
-        print "  applying norm", normFactor
+        #pass
+        #print "Finalize:"
+        #normFactor = self.getNormalizationFactor()
+        #print "  applying norm", normFactor
+        self.avgW /= self.cnt
         for h in self.histos:
-            self.histos[h][1].Scale(normFactor)
+            self.histos[h][1].Scale(1/self.avgW)
 
 if __name__ == "__main__":
     sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
@@ -81,21 +144,27 @@ if __name__ == "__main__":
     maxFilesMC = None
     nWorkers = None
 
-    '''
-    sampleList = ["QCD_Pt-30to50_Tune4C_13TeV_pythia8",]
+    #'''
+    #sampleList = ["QCD_Pt-30to50_Tune4C_13TeV_pythia8",]
     #sampleList = ["QCD_Pt-10to15_Tune4C_13TeV_pythia8",]
-    maxFilesMC = 1
-    nWorkers = 1
+    #maxFilesMC = 12
+    #nWorkers = 12
     # '''
     #maxFilesMC = 32
 
     slaveParams = {}
 
     # select hltCollection here (see plugins/MNTriggerAna.cc to learn whats avaliable):
-    slaveParams["hltCollection"] = "hltAK5PFJetL1FastL2L3Corrected"
+    #slaveParams["hltCollection"] = "hltAK5PFJetL1FastL2L3Corrected"
+    #slaveParams["hltCollection"] = "hltPFJetsCorrectedMatchedToL1"
+    slaveParams["hltCollection"] = "hltAK4PFJetsCorrected"
+
+    slaveParams["recoCollection"] = "PFAK4CHSnewjets"
+    
+
 
     # note - remove maxFiles parameter in order to run on all files
-    HLTRate.runAll(treeName="mnTriggerAna", 
+    HLTRate.runAll(treeName="MNTriggerAnaNew",
                                slaveParameters=slaveParams,
                                sampleList=sampleList,
                                maxFilesMC = maxFilesMC,
