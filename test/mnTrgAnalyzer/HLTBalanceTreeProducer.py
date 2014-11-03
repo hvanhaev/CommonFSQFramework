01dd6aeee6a5dad8de8f4fabe470919206fafdb1
Tomasz <fruboes@cern.ch>
2014-11-03 11:15:33 +0100

    Standalone mode, lots of changes

--- test/mnTrgAnalyzer/HLTBalanceTreeProducer.py
+++ test/mnTrgAnalyzer/HLTBalanceTreeProducer.py
@@ -22,30 +22,51 @@ from ROOT import edm, JetCorrectionUncertainty
 import BalanceTreeProducer 
 from MNTriggerStudies.MNTriggerAna.JetGetter import JetGetter
 
+import BaseTrigger
+
+
 class HLTBalanceTreeProducer(BalanceTreeProducer.BalanceTreeProducer):
     def init(self):
         BalanceTreeProducer.BalanceTreeProducer.init(self)
         self.addExternalVar(["hltPtAve"])
         self.addExternalVar(["hltPtCen"])
         self.addExternalVar(["hltPtFwd"])
-        self.addExternalVar(["hltL1MatchPtCen"])
-        self.addExternalVar(["hltL1MatchPtFwd"])
-        self.addExternalVar(["l1DoubleJet"])
-        self.addExternalVar(["l1SingleJetCentral"])
-        self.addExternalVar(["l1SingleJetForward"])
+        self.addExternalVar(["hltCaloPtAve"])
+        self.addExternalVar(["hltCaloPtCen"])
+        self.addExternalVar(["hltCaloPtFwd"])
+
+        #self.addExternalVar(["hltL1MatchPtCen"])
+        #self.addExternalVar(["hltL1MatchPtFwd"])
+        #self.addExternalVar(["l1DoubleJet"])
+        #self.addExternalVar(["l1SingleJetCentral"])
+        #self.addExternalVar(["l1SingleJetForward"])
         self.addExternalVar(["s1l1SingleJetCentral"])
         self.addExternalVar(["s1l1SingleJetForward"])
+        self.addExternalVar(["s1l1SingleJetAny"])
+
+        hltgetter = BaseTrigger.TriggerObjectsGetter(self.fChain, "hltAK4PFJetsCorrected")
+        self.hltAveFromPython = BaseTrigger.PTAveProperTrigger(hltgetter)
+        self.addExternalVar(["hltAveFromPython"])
+        self.addExternalVar(["hltCaloPreselection"])
 
 
+
+
+        '''
         self.addExternalVar(["s1DoubleJetCFDphi31"])
         self.addExternalVar(["s1DoubleJetCFDphi27"])
         self.addExternalVar(["s1DoubleJetCFDphi24"])
         self.addExternalVar(["s1DoubleJetCFDphi20"])
         self.addExternalVar(["s1DoubleJetCFDphi17"])
+        '''
 
 
         self.addExternalVar(["PUNumInteractions"])
         self.addExternalVar(["puTrueNumInteractions"])
+
+        self.addExternalVar(["trgptAve60CenFwd"])
+        #self.addExternalVar(["trgptAve80CenFwd"])
+
 
 
         # for the PU file run
@@ -53,7 +74,7 @@ class HLTBalanceTreeProducer(BalanceTreeProducer.BalanceTreeProducer):
         puFile = edm.FileInPath("MNTriggerStudies/MNTriggerAna/test/mnTrgAnalyzer/PUhists.root").fullPath()
 
         self.newlumiWeighters = {}
-        #'''
+        '''
         self.newlumiWeighters["flat010toflat010"] = edm.LumiReWeighting(puFile, puFile, "Flat0to10/pileup", "Flat0to10/pileup")
         self.newlumiWeighters["flat010toPU1"] = edm.LumiReWeighting(puFile, puFile, "Flat0to10/pileup", "PU1/pileup")
         #self.newlumiWeighters["flat010toPU2"] = edm.LumiReWeighting(puFile, puFile, "Flat0to10/pileup", "PU2/pileup")
@@ -62,7 +83,7 @@ class HLTBalanceTreeProducer(BalanceTreeProducer.BalanceTreeProducer):
         self.newlumiWeighters["flat010toPU5"] = edm.LumiReWeighting(puFile, puFile, "Flat0to10/pileup", "PU5/pileup")
         self.newlumiWeighters["flat010toPU10"] = edm.LumiReWeighting(puFile, puFile, "Flat0to10/pileup", "PU10/pileup")
         #'''
-        '''
+        #'''
         self.newlumiWeighters["flat2050toPU20"] = edm.LumiReWeighting(puFile, puFile, "Flat20to50/pileup", "PU20/pileup")
         self.newlumiWeighters["flat2050toPU25"] = edm.LumiReWeighting(puFile, puFile, "Flat20to50/pileup", "PU25/pileup")
         self.newlumiWeighters["flat2050toPU30"] = edm.LumiReWeighting(puFile, puFile, "Flat20to50/pileup", "PU30/pileup")
@@ -110,6 +131,37 @@ class HLTBalanceTreeProducer(BalanceTreeProducer.BalanceTreeProducer):
 
         return (tag, probe)
 
+    def emulateHLTAveNew(self, l1jets, hltJets, minJetPT, l1thr, aveMin):
+        for j in l1jets:
+            if j.pt() > l1thr:
+                break
+        else:
+            return 0
+
+        probes = []
+        tags = []
+
+        for j in hltJets:
+            pt = j.pt()
+            if pt < minJetPT: continue
+            eta = abs(j.eta())
+            if eta < 1.4: tags.append(j)
+            if eta > 2.7 and eta < 5.5: probes.append(j)
+
+        if not probes or not tags: return 0
+        bestAve = 0
+        for t in tags:
+            for p in probes:
+                dphi = abs(ROOT.Math.VectorUtil.DeltaPhi(t, p))
+                if dphi < 1.5: continue
+                ave = (p.pt()+t.pt())/2
+                if ave > bestAve: bestAve = ave
+
+
+            
+        if bestAve > aveMin: return 1
+        return 0
+
     def analyzeDumb(self):
     #def analyze(self):
         self.setExternalVar("PUNumInteractions", self.fChain.PUNumInteractions)
@@ -127,62 +179,129 @@ class HLTBalanceTreeProducer(BalanceTreeProducer.BalanceTreeProducer):
 
     #def analyzeGood(self):
     def analyze(self):
-        tag, probe = self.getHLTTagProbe(self.fChain.hltAK4PFJetsCorrected)
-        tagL1match, probeL1match = self.getHLTTagProbe(self.fChain.hltPFJetsCorrectedMatchedToL1)
+        thr = self.hltAveFromPython.getMaxThreshold()
+        self.setExternalVar("hltAveFromPython", thr)
+        #     cut = cms.string( "pt>40 && (abs(eta)<1.4 || abs(eta) > 2.7)" )
+        bestCalo = 0
+        for j in self.fChain.hltAK4CaloJetsCorrected:
+            eta = abs(j.eta())
+            if eta > 1.4 and eta < 2.7:
+                continue
+            pt = j.pt()
+            if pt > bestCalo: bestCalo = pt
+
+        self.setExternalVar("hltCaloPreselection", bestCalo)
 
 
-        avePT = 0 # this are all HLT level values!
-        tagPT = 0 
-        probePT = 0
-        tagL1matchPT = 0 
-        probeL1matchPT = 0
-        
-        if probe != None and tag!=None:  
-            #print "XXX", tag.pt(), probe.pt()
-            avePT = (tag.pt() + probe.pt())/2.
-            tagPT = tag.pt()
-            probePT = probe.pt()
 
-        if probeL1match != None:
-            probeL1matchPT = probeL1match.pt()
+        '''
+        #s1l1Jets = self.fChain.stage1L1Jets
+        tmpl1 = self.fChain.oldL1Jets
+        #tmpl1 = self.fChain.stage1L1Jets
+        tmphlt =  self.fChain.hltAK4PFJetsCorrected
+        #     def emulateHLTAveNew(self, l1jets, hltJets, minJetPT, l1thr, ave):
+        res60 = self.emulateHLTAveNew(tmpl1, tmphlt, 30, 34, 60)
+        res80 = self.emulateHLTAveNew(tmpl1, tmphlt, 20, 66, 40)
 
-        if tagL1match != None:
-            tagL1matchPT = tagL1match.pt()
+        dbg = False
+        if abs(res60-self.fChain.trgnewAve60) > 0.1: 
+            print "XXXProblem 60: ", res60, self.fChain.trgnewAve60
+            dbg = True
+
+        else: 
+            print "YYYOK 60"
+        if abs(res80-self.fChain.trgnewAve80) > 0.1: 
+            print "XXXProblem 80: ", res60, self.fChain.trgnewAve80
+            dbg = True
+        else: 
+            print "YYYOK 80"
+
+        if dbg: 
+            for l1 in tmpl1:
+                print "l1", l1.pt()
+            for hlt in tmphlt:
+                if hlt.pt() < 29: continue
+                print "hlt", hlt.pt(), hlt.eta(), hlt.phi()
+
+        return
+        #'''
+
+        todo = {}
+        todo["hlt"] = self.getHLTTagProbe(self.fChain.hltAK4PFJetsCorrected)
+        #todo["hltL1match"] = self.getHLTTagProbe(self.fChain.hltAK4PFJetsCorrected)
+        todo["hltCalo"] = self.getHLTTagProbe(self.fChain.hltAK4CaloJetsCorrected)
+        #tag, probe = self.getHLTTagProbe(self.fChain.hltAK4PFJetsCorrected)
+        #tagCalo, probeCalo = self.getHLTTagProbe(self.fChain.hltAK4CaloJetsCorrected)
+        #tagL1match, probeL1match = self.getHLTTagProbe(self.fChain.hltPFJetsCorrectedMatchedToL1)
+
+        for t in todo:
+            avePT = 0 # this are all HLT level values!
+            tagPT = 0 
+            probePT = 0
+            tagL1matchPT = 0 
+            probeL1matchPT = 0
+            tag = todo[t][0]
+            probe = todo[t][1]
+            
+            if probe != None and tag!=None:  
+                #print "XXX", tag.pt(), probe.pt()
+                avePT = (tag.pt() + probe.pt())/2.
+                tagPT = tag.pt()
+                probePT = probe.pt()
+
+            #print t, tagPT, probePT
+            self.setExternalVar(t+"PtAve", avePT)
+            self.setExternalVar(t+"PtCen", tagPT)
+            self.setExternalVar(t+"PtFwd", probePT)
 
 
-        self.setExternalVar("hltL1MatchPtCen", tagL1matchPT)
-        self.setExternalVar("hltL1MatchPtFwd", probeL1matchPT)
 
-
-        self.setExternalVar("hltPtAve", avePT)
-        self.setExternalVar("hltPtCen", tagPT)
-        self.setExternalVar("hltPtFwd", probePT)
         self.setExternalVar("PUNumInteractions", self.fChain.PUNumInteractions)
         self.setExternalVar("puTrueNumInteractions", self.fChain.puTrueNumInteractions)
 
+        self.setExternalVar("trgptAve60CenFwd", self.fChain.trgptAve60CenFwd)
+        #self.setExternalVar("trgptAve80CenFwd", self.fChain.trgptAve80CenFwd)
 
-        l1Jets = self.fChain.l1Jets
+        #l1Jets = self.fChain.l1Jets
         s1l1Jets = self.fChain.stage1L1Jets
         #print "XX"
         todo = {}
-        todo["old"] = l1Jets
+        #todo["old"] = l1Jets
         todo["stage1"] = s1l1Jets
 
         #  0.0 0.34906578064 0.698131561279 1.04719740549 1.39626330534 1.74532920519 2.09439510107 2.44346088568 2.79252672195 3.14159256617
-        self.setExternalVar("s1DoubleJetCFDphi31", self.getBestL1PairWithPhiSeparation(s1l1Jets, 3.1))
-        self.setExternalVar("s1DoubleJetCFDphi27", self.getBestL1PairWithPhiSeparation(s1l1Jets, 2.7))
-        self.setExternalVar("s1DoubleJetCFDphi24", self.getBestL1PairWithPhiSeparation(s1l1Jets, 2.4))
-        self.setExternalVar("s1DoubleJetCFDphi20", self.getBestL1PairWithPhiSeparation(s1l1Jets, 2.0))
-        self.setExternalVar("s1DoubleJetCFDphi17", self.getBestL1PairWithPhiSeparation(s1l1Jets, 1.7))
+        ##self.setExternalVar("s1DoubleJetCFDphi31", self.getBestL1PairWithPhiSeparation(s1l1Jets, 3.1))
+        #self.setExternalVar("s1DoubleJetCFDphi27", self.getBestL1PairWithPhiSeparation(s1l1Jets, 2.7))
+        ##self.setExternalVar("s1DoubleJetCFDphi24", self.getBestL1PairWithPhiSeparation(s1l1Jets, 2.4))
+        #self.setExternalVar("s1DoubleJetCFDphi20", self.getBestL1PairWithPhiSeparation(s1l1Jets, 2.0))
+        #self.setExternalVar("s1DoubleJetCFDphi17", self.getBestL1PairWithPhiSeparation(s1l1Jets, 1.7))
 
 
         for t in todo:
             pts = []
             hardestL1Central = 0
             hardestL1Forwad  = 0
+            hardestL1  = 0
 
             # eta l1 scale:  -0.174 -0.5215 -0.8695 -1.218 -1.566 -1.956 -2.586 -3.25 -3.75 -4.25 -4.75
             for j in todo[t]:
+                pt = j.pt()
+                if pt > hardestL1: hardestL1 = pt
+                eta = abs(j.eta())
+                if abs(j.eta()) < 1.7 and hardestL1Central < j.pt():
+                    hardestL1Central = j.pt()
+                if abs(j.eta()) > 2.5 and hardestL1Forwad < j.pt():
+                    hardestL1Forwad = j.pt()
+
+
+
+            if t == "stage1":
+                #print "L1", hardestL1
+                self.setExternalVar("s1l1SingleJetAny", hardestL1)
+                self.setExternalVar("s1l1SingleJetCentral", hardestL1Central)
+                self.setExternalVar("s1l1SingleJetForward", hardestL1Forwad)
+
+            '''
                 #print "PPP", j.phi()
                 pts.append(j.pt())
                 if abs(j.eta()) < 1.7 and hardestL1Central < j.pt():
@@ -205,8 +324,10 @@ class HLTBalanceTreeProducer(BalanceTreeProducer.BalanceTreeProducer):
             elif t == "stage1":
                 self.setExternalVar("s1l1SingleJetCentral", hardestL1Central)
                 self.setExternalVar("s1l1SingleJetForward", hardestL1Forwad)
+                self.setExternalVar("s1l1SingleJetAny", hardestL1)
             else:
                 raise Exception("thats surprising!")
+            '''
 
         pu = self.fChain.PUNumInteractions
         genW = BalanceTreeProducer.BalanceTreeProducer.genWeight(self)
@@ -214,8 +335,13 @@ class HLTBalanceTreeProducer(BalanceTreeProducer.BalanceTreeProducer):
             w = self.newlumiWeighters[l].weight(pu)
             self.setExternalVar(l, w*genW)
 
-
-        BalanceTreeProducer.BalanceTreeProducer.analyze(self)
+        if self.standalone:
+            BalanceTreeProducer.BalanceTreeProducer.setExternals(self)
+            BalanceTreeProducer.BalanceTreeProducer.fillGenWeight(self)
+            BalanceTreeProducer.BalanceTreeProducer.fill(self)
+            BalanceTreeProducer.BalanceTreeProducer.resetExternals(self)
+        else:
+            BalanceTreeProducer.BalanceTreeProducer.analyze(self)
 
     def finalize(self):
         print "Finalize HLTBala:"
@@ -245,11 +371,12 @@ if __name__ == "__main__":
     slaveParams["HLT2015TempWorkaround"] =  True
     slaveParams["doPtShiftsJER"] = False
     slaveParams["doPtShiftsJEC"] = False
+    slaveParams["standalone"] = True
 
 
+    sampleList=["QCD_Pt-15to3000_Tune4C_Flat_13TeV_pythia8"]
     '''
-    sampleList=["QCD_Pt-300to470_Tune4C_13TeV_pythia8"]
-    nWorkers = 1
+    nWorkers = 12
     maxFilesMC = 12
     #'''
     #maxFilesMC = 1
