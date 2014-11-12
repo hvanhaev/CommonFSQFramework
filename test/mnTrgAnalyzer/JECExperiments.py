be1833161ab100281312fae3373cd01538fec578
Tomasz <fruboes@cern.ch>
2014-11-12 11:24:38 +0100

    Plotting version

--- test/mnTrgAnalyzer/JECExperiments.py
+++ test/mnTrgAnalyzer/JECExperiments.py
@@ -9,15 +9,31 @@ import ROOT
 ROOT.gROOT.SetBatch(True)
 from ROOT import edm
 
+from itertools import ifilter
+
+
 from array import *
 
 class JECExperiments(MNTriggerStudies.MNTriggerAna.ExampleProofReader.ExampleProofReader):
     def init( self):
-        self.jets = GenericGetter("hltAK4PFJetsCorrected", "eta") 
+        #self.jetBranch = "hltAK4PFJetsCorrected"
+        #self.jetBranch = "hltAK4PFJets"
+        self.jetBranch = "pfAK4CHS"
+        self.jets = GenericGetter(self.jetBranch, "eta") 
 
         self.hist = {}
         self.hist["pt"] =  ROOT.TH1F("pt",   "pt",  100, -0.5, 99.5)
+        self.hist["ptGen"] =  ROOT.TH1F("ptGen",   "ptGen",  100, -0.5, 99.5)
+        self.hist["rho"] =  ROOT.TH1F("rho",   "rho",  100, -0.5, 99.5)
         self.hist["eta"] =  ROOT.TH1F("eta",   "eta",  100, -5.5, 5.5)
+        self.hist["bestdr"] =  ROOT.TH1F("bestdr",   "bestdr",  100, 0, 2)
+        self.hist["ptGenVsPtRec"] = ROOT.TH2F("ptGenVsPtRec", "ptGenVsPtRec", 100, 0, 100, 100, 0, 100)
+        self.hist["ptGenVsPtRecPR"] = ROOT.TProfile("ptGenVsPtRecPR", "ptGenVsPtRecPR", 100, 0, 1000, 0, 1000)
+    
+        self.hist["response"] =  ROOT.TH1F("response",   "response",  100, 0, 2)
+        self.hist["responseVsGenPT"] = ROOT.TProfile("responseVsGenPT", "responseVsGenPT", 100, 0, 100, 0, 10)
+        self.hist["responseVsEta"] = ROOT.TProfile("responseVsEta", "responseVsEta", 20, -5.5, 5.5, 0, 10)
+        self.hist["responseVsPU"] = ROOT.TProfile("responseVsPU", "responseVsPU", 20, 0, 100, 0, 10)
         '''
             area
             bestdr
@@ -33,22 +49,52 @@ class JECExperiments(MNTriggerStudies.MNTriggerAna.ExampleProofReader.ExamplePro
             self.GetOutputList().Add(self.hist[h])
 
     def analyze(self):
-        weight = 1
+        rho =  getattr(self.fChain, self.jetBranch+"rho")
+        if rho < 28 or rho > 32: return
+
+        weight =  self.fChain.genWeight
         self.jets.newEvent(self.fChain)
+        PU = self.fChain.PUNumInteractions 
+
         if self.jets:
-            hardestJet = max(self.jets.get(""), key = lambda j: j.pt)
-            self.hist["pt"].Fill(hardestJet.pt, weight)
-            self.hist["eta"].Fill(hardestJet.eta, weight)
+            # rank jets by pt
+            jselect = lambda j: 1 if abs(j.eta) < 0.2 \
+                            and j.pt > 40  \
+                            and j.bestdr < 0.25 \
+                            else 0
+
+            rankAndSelect = lambda j: j.ptGen*jselect(j)
+            #hardestJet = max(self.jets.get(""), key = rankAndSelect)
+            #if not rankAndSelect(jet): continue
+            for jet in ifilter(jselect, self.jets.get("")):
+                pt = jet.pt
+                eta = jet.eta
+                ptGen = jet.ptGen
+                self.hist["pt"].Fill(pt, weight)
+                self.hist["ptGen"].Fill(ptGen, weight)
+                self.hist["bestdr"].Fill(jet.bestdr, weight)
+                self.hist["eta"].Fill(eta, weight)
+                self.hist["rho"].Fill(rho, weight)
+
+                self.hist["ptGenVsPtRec"].Fill(pt, ptGen, weight)
+                self.hist["ptGenVsPtRecPR"].Fill(pt, ptGen, weight)
+
+                r = jet.ptGenRatio
+                self.hist["response"].Fill(r, weight)
+                self.hist["responseVsGenPT"].Fill(ptGen, r, weight) 
+                self.hist["responseVsEta"].Fill(eta, r, weight)
+                self.hist["responseVsPU"].Fill(PU, r, weight)
 
         return
 
 
     def finalize(self):
-        print "Finalize:"
-        normFactor = self.getNormalizationFactor()
-        print "  applying norm", normFactor
-        for h in self.hist:
-            self.hist[h].Scale(normFactor)
+        pass
+        #print "Finalize:"
+        #normFactor = self.getNormalizationFactor()
+        #print "  applying norm", normFactor
+        #for h in self.hist:
+        #    self.hist[h].Scale(normFactor)
 
 if __name__ == "__main__":
     sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
@@ -64,9 +110,9 @@ if __name__ == "__main__":
     # Run printTTree.py alone to get the samples list
     #sampleList = []
     #sampleList.append("QCD_Pt-15to3000_TuneZ2star_Flat_HFshowerLibrary_7TeV_pythia6")
-    maxFilesMC = 1
-    maxFilesData = 1
-    nWorkers = 1
+    #maxFilesData = 1
+    #maxFilesMC = 1
+    #nWorkers = 1
 
 
     slaveParams = {}
@@ -74,7 +120,8 @@ if __name__ == "__main__":
 
 
     # use printTTree.py <sampleName> to see what trees are avaliable inside the skim file
-    JECExperiments.runAll(treeName="BFJecTreeProducerHighPT",
+    #JECExperiments.runAll(treeName="BFJecTreeProducerHighPT",
+    JECExperiments.runAll(treeName="BFJecTreeProducer",
            slaveParameters=slaveParams,
            sampleList=sampleList,
            maxFilesMC = maxFilesMC,
