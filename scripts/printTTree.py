09e3aaa60ff7f87ec0f01e10ef3e802bd4721173
Tomasz <fruboes@cern.ch>
2014-11-19 13:53:06 +0100

    Do not verify all datasets

--- scripts/printTTree.py
+++ scripts/printTTree.py
@@ -8,6 +8,7 @@ import os,re
 from optparse import OptionParser
 
 from MNTriggerStudies.MNTriggerAna.GetDatasetInfo import getTreeFilesAndNormalizations
+from MNTriggerStudies.MNTriggerAna.Util import getAnaDefinition
 
 
 def main():
@@ -18,15 +19,18 @@ def main():
     #parser.add_option("-s", "--sample",   action="store", type="string", dest="sample", help="sample name" )
     #parser.add_option("-l", "--listSamples",   action="store", type="string", dest="list", help="listAllSamples" )
     (options, args) = parser.parse_args()
-    treeFilesAndNormalizations = getTreeFilesAndNormalizations(maxFilesMC=1, maxFilesData=1, quiet = True)
-    if len(args) != 1 or args[0] not in treeFilesAndNormalizations:
+
+    anaDef = getAnaDefinition("sam")
+    if len(args) != 1 or args[0] not in anaDef:
         print "Usage: printTTree.py sampleName"
         print "Avaliable samples:"
-        for t in treeFilesAndNormalizations:
+        for t in anaDef:
             print " ", t
         sys.exit(0)
 
     sample= args[0]
+    treeFilesAndNormalizations = getTreeFilesAndNormalizations(maxFilesMC=1, maxFilesData=1,
+                quiet = True, samplesToProcess=[sample,])
     filename = treeFilesAndNormalizations[sample]["files"][0]
 
     
