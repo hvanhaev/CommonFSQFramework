0eb688cdaf38fd82bc71908f84bd5b8d117577fb
Tomasz <fruboes@cern.ch>
2015-02-04 14:11:40 +0100

    Fix when running ober multiple QCDs

--- test/MNxsectionAna/fitPtHatReweighing.py
+++ test/MNxsectionAna/fitPtHatReweighing.py
@@ -18,11 +18,14 @@ def main():
     for s in dataDSNames:
         lumi += sampleList[s]["lumiJet15"]
 
+    print "Lumi:", lumi
+
     todoQCDNames = ["QCD_Pt-15to3000_TuneZ2star_Flat_HFshowerLibrary_7TeV_pythia6", "QCD_Pt-15to1000_TuneEE3C_Flat_7TeV_herwigpp"]
     #todoQCDNames = ["QCD_Pt-15to1000_TuneEE3C_Flat_7TeV_herwigpp",]
+    #todoQCDNames = ["QCD_Pt-15to3000_TuneZ2star_Flat_HFshowerLibrary_7TeV_pythia6"]
     
     hData = ROOT.TH1F("dataa", "dataa", 100, 0, 100)
-    hMC = hData.Clone()
+    hMCbase = hData.Clone()
 
     data[0].fillHistogram(hData, ROOT.RooArgList(data[1]["leadPt"]))
     hData.Scale(1./lumi)
@@ -30,23 +33,27 @@ def main():
     c = ROOT.TCanvas()
     outFile = ROOT.TFile("ptHatWeighters.root", "recreate")
     for t in todoQCDNames:
-
         xxx = outFile.mkdir(t)
-        dMC = getSummedRooDS("t", "treesForPTHatReweighing.root", [t], "weight")
+        hMC = hMCbase.Clone("MC_"+t)
+        dMC = getSummedRooDS(t, "treesForPTHatReweighing.root", [t], "weight")
         dMC[0].fillHistogram(hMC, ROOT.RooArgList(dMC[1]["leadPt"]))
         hMC.Draw()
         hMC.SetLineColor(2)
         hData.Draw("SAME")
         c.Print("~/tmp/ttt.png")
+        
+        hData2MC = hData.Clone("data2mc_"+t)
 
-        hData.Divide(hMC)
+        hData2MC.Divide(hMC)
 
         fitF = ROOT.TF1("ptHatW","[0]*x + [1]", 0, 1000000);
         fitF.SetParameter(0, 0.)
         fitF.SetParameter(1, 1.)
-        hData.Fit("ptHatW")
+        hData2MC.Fit("ptHatW")
 
+        xxx.WriteTObject(hData2MC)
         xxx.WriteTObject(hData)
+        xxx.WriteTObject(hMC)
         xxx.WriteTObject(fitF)
 
 #       leadEta
