8391720e710c89976415c87cf61edcdf188ff546
Tomasz <fruboes@cern.ch>
2015-02-06 16:22:51 +0100

    Towards minuit

--- test/MNxsectionAna/fitPtHatReweighing.py
+++ test/MNxsectionAna/fitPtHatReweighing.py
@@ -9,6 +9,39 @@ import MNTriggerStudies.MNTriggerAna.Util
 
 from  RooDSHelper import getSummedRooDS
 
+def doMinuitFit(ofile, data, mc):
+    pass
+
+def doBaselineFit(ofile, dsData, dsMC, lumi):
+    c = ROOT.TCanvas()
+    hData = ROOT.TH1F("dataa", "dataa", 100, 0, 100)
+    hMCbase = hData.Clone()
+    dsData[0].fillHistogram(hData, ROOT.RooArgList(dsData[1]["leadPt"]))
+    hData.Scale(1./lumi)
+
+    hMC = hMCbase.Clone("MC")
+    dsMC[0].fillHistogram(hMC, ROOT.RooArgList(dsMC[1]["leadPt"]))
+    hMC.Draw()
+    hMC.SetLineColor(2)
+    hData.Draw("SAME")
+    c.Print("~/tmp/ttt.png")
+    
+    hData2MC = hData.Clone("data2mc")
+
+    hData2MC.Divide(hMC)
+
+    fitF = ROOT.TF1("ptHatW","[0]/x + [1]", 0, 1000000);
+    fitF.SetParameter(0, 0.)
+    fitF.SetParameter(1, 1.)
+    hData2MC.Fit("ptHatW")
+
+    ofile.WriteTObject(hData2MC)
+    ofile.WriteTObject(hData)
+    ofile.WriteTObject(hMC)
+    ofile.WriteTObject(fitF)
+
+
+
 
 def main():
     dataDSNames = ["JetMETTau-Run2010A-Apr21ReReco-v1", "Jet-Run2010B-Apr21ReReco-v1", "JetMET-Run2010A-Apr21ReReco-v1"]
@@ -23,38 +56,12 @@ def main():
     todoQCDNames = ["QCD_Pt-15to3000_TuneZ2star_Flat_HFshowerLibrary_7TeV_pythia6", "QCD_Pt-15to1000_TuneEE3C_Flat_7TeV_herwigpp"]
     #todoQCDNames = ["QCD_Pt-15to1000_TuneEE3C_Flat_7TeV_herwigpp",]
     #todoQCDNames = ["QCD_Pt-15to3000_TuneZ2star_Flat_HFshowerLibrary_7TeV_pythia6"]
-    
-    hData = ROOT.TH1F("dataa", "dataa", 100, 0, 100)
-    hMCbase = hData.Clone()
 
-    data[0].fillHistogram(hData, ROOT.RooArgList(data[1]["leadPt"]))
-    hData.Scale(1./lumi)
-
-    c = ROOT.TCanvas()
     outFile = ROOT.TFile("ptHatWeighters.root", "recreate")
     for t in todoQCDNames:
         xxx = outFile.mkdir(t)
-        hMC = hMCbase.Clone("MC_"+t)
         dMC = getSummedRooDS(t, "treesForPTHatReweighing.root", [t], "weight")
-        dMC[0].fillHistogram(hMC, ROOT.RooArgList(dMC[1]["leadPt"]))
-        hMC.Draw()
-        hMC.SetLineColor(2)
-        hData.Draw("SAME")
-        c.Print("~/tmp/ttt.png")
-        
-        hData2MC = hData.Clone("data2mc_"+t)
-
-        hData2MC.Divide(hMC)
-
-        fitF = ROOT.TF1("ptHatW","[0]/x + [1]", 0, 1000000);
-        fitF.SetParameter(0, 0.)
-        fitF.SetParameter(1, 1.)
-        hData2MC.Fit("ptHatW")
-
-        xxx.WriteTObject(hData2MC)
-        xxx.WriteTObject(hData)
-        xxx.WriteTObject(hMC)
-        xxx.WriteTObject(fitF)
+        doBaselineFit(xxx, data, dMC, lumi)
 
 #       leadEta
 #       leadPt
