0282566b162576fa35f9b1c58f35036d8ba76e96
Tomasz <fruboes@cern.ch>
2015-02-06 17:56:31 +0100

    Non crashing version of minuit fit

--- test/MNxsectionAna/fitPtHatReweighing.py
+++ test/MNxsectionAna/fitPtHatReweighing.py
@@ -2,20 +2,98 @@
 
 import ROOT
 ROOT.gROOT.SetBatch(True)
+#ROOT.gSystem.Load("libRooFit.so" ) # for bindFunction
+#https://root.cern.ch/root/htmldoc/tutorials/roofit/rf105_funcbinding.C.html
 
-import os,sys
+import os,sys, math
+from array import array
 
 import MNTriggerStudies.MNTriggerAna.Util
 
-from  RooDSHelper import getSummedRooDS
+from  RooDSHelper import getSummedRooDS, reweighDS
 
-def doMinuitFit(ofile, data, mc):
-    pass
+# I hate myself...
+hData = ROOT.TH1F("dataa", "dataa", 100, 0, 100)
+hMCbase = hData.Clone()
+globalMC = None
+
+def doMinuitFit(ofile, dsData, dsMC, lumi):
+    c = ROOT.TCanvas()
+    hData.Reset()
+    dsData[0].fillHistogram(hData, ROOT.RooArgList(dsData[1]["leadPt"]))
+    hData.Scale(1./lumi)
+    global globalMC
+    globalMC = dsMC
+
+    # setup minuit
+    gMinuit = ROOT.TMinuit(5)
+    gMinuit.SetFCN( fcn )
+
+    arglist = array( 'd', 10*[0.] )
+    ierflg = ROOT.Long(1982)
+
+    arglist[0] = 1
+    gMinuit.mnexcm( "SET ERR", arglist, 1, ierflg )
+
+    # Set starting values and step sizes for parameters
+    vstart = array( 'd', ( 1,  0) )
+    step   = array( 'd', ( 0.01, 0.01 ) )
+    gMinuit.mnparm( 0, "a1", vstart[0], step[0], 0, 0, ierflg )
+    gMinuit.mnparm( 1, "a2", vstart[1], step[1], 0, 0, ierflg )
+
+    # Now ready for minimization step
+    arglist[0] = 500 # max calls 
+    arglist[1] = 1.  # tolerance - how far from minimum
+    gMinuit.mnexcm( "MIGRAD", arglist, 2, ierflg )
+
+    # Print results
+    amin, edm, errdef = ROOT.Double(0.18), ROOT.Double(0.19), ROOT.Double(0.20)
+    nvpar, nparx, icstat = ROOT.Long(1983), ROOT.Long(1984), ROOT.Long(1985)
+    gMinuit.mnstat( amin, edm, errdef, nvpar, nparx, icstat )
+
+cnt = 0
+
+def fcn( npar, gin, f, par, iflag ):
+    global cnt
+    cnt +=1
+    # get weighted MC ds
+    global globalMC
+    vars = globalMC[1]
+    #localMCds = globalMC[0].Clone()
+    baseWeight = "weight" # yuck, Q&D
+    a1 = ROOT.RooRealVar("a1","a1", par[0])
+    a2 = ROOT.RooRealVar("a2","a2", par[1])
+    args = ROOT.RooArgList(vars["qScale"], a1, a2)
+    linear = ROOT.RooFormulaVar("lin", "lin", "a1+a2*qScale", args)
+
+    newweight = ROOT.RooFormulaVar("w"+str(cnt), "ww", baseWeight+"*lin"  , ROOT.RooArgList(vars[baseWeight], linear))
+    wds = reweighDS(globalMC[0], "neww", newweight)
+
+
+    # fill MC histograms
+    hMC = hMCbase.Clone("MC")
+    wds.fillHistogram(hMC, ROOT.RooArgList(vars["leadPt"]))
+
+    # get chi2
+    chisq, delta = 0., 0.
+    for i in xrange(1, hMC.GetNbinsX()+1):
+        errData = hData.GetBinError(i)
+        errMC =   hMC.GetBinError(i)
+        err = math.sqrt(errData*errData+errMC*errMC)
+        if err > 0:
+            delta = (hData.GetBinContent(i)-hMC.GetBinError(i))/err
+            chisq += delta*delta
+        
+    f[0] = chisq
+    print "Call:", cnt, par[0], par[1], "chisq", chisq
+
+
+
+
 
 def doBaselineFit(ofile, dsData, dsMC, lumi):
     c = ROOT.TCanvas()
-    hData = ROOT.TH1F("dataa", "dataa", 100, 0, 100)
-    hMCbase = hData.Clone()
+    hData.Reset()
     dsData[0].fillHistogram(hData, ROOT.RooArgList(dsData[1]["leadPt"]))
     hData.Scale(1./lumi)
 
@@ -53,15 +131,17 @@ def main():
 
     print "Lumi:", lumi
 
-    todoQCDNames = ["QCD_Pt-15to3000_TuneZ2star_Flat_HFshowerLibrary_7TeV_pythia6", "QCD_Pt-15to1000_TuneEE3C_Flat_7TeV_herwigpp"]
-    #todoQCDNames = ["QCD_Pt-15to1000_TuneEE3C_Flat_7TeV_herwigpp",]
+    #todoQCDNames = ["QCD_Pt-15to3000_TuneZ2star_Flat_HFshowerLibrary_7TeV_pythia6", "QCD_Pt-15to1000_TuneEE3C_Flat_7TeV_herwigpp"]
+    todoQCDNames = ["QCD_Pt-15to1000_TuneEE3C_Flat_7TeV_herwigpp",]
     #todoQCDNames = ["QCD_Pt-15to3000_TuneZ2star_Flat_HFshowerLibrary_7TeV_pythia6"]
 
     outFile = ROOT.TFile("ptHatWeighters.root", "recreate")
     for t in todoQCDNames:
         xxx = outFile.mkdir(t)
         dMC = getSummedRooDS(t, "treesForPTHatReweighing.root", [t], "weight")
-        doBaselineFit(xxx, data, dMC, lumi)
+
+        #doBaselineFit(xxx, data, dMC, lumi)
+        doMinuitFit(xxx, data, dMC, lumi)
 
 #       leadEta
 #       leadPt
