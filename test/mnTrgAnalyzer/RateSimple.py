21d03dfec3412e231f4bb64f18757667b892e46d
Tomasz <fruboes@cern.ch>
2015-03-06 07:50:33 +0100

    Use external functions for trigger emulation

--- test/mnTrgAnalyzer/RateSimple.py
+++ test/mnTrgAnalyzer/RateSimple.py
@@ -23,17 +23,24 @@ class RateSimple(MNTriggerStudies.MNTriggerAna.ExampleProofReader.ExampleProofRe
     def init(self):
         self.cnt = 0
 
-        self.dist = ROOT.TH1F("dist", "; jet seed threshold", 21, -0.5,20.5 )
+        binL = -0.5
+        binH = 61.5
+        nbins = int(binH-binL)
+
+        self.dist = ROOT.TH1F("dist", "; jet seed threshold", nbins, binL, binH )
         self.dist.Sumw2()
         self.GetOutputList().Add(self.dist)
 
         puFile = edm.FileInPath("MNTriggerStudies/MNTriggerAna/test/mnTrgAnalyzer/PUhists.root").fullPath()
         self.newlumiWeighters = {}
+        '''
+        self.newlumiWeighters["flat2050toPU40"] = edm.LumiReWeighting(puFile, puFile, "Flat20to50/pileup", "PU40/pileup")
         self.newlumiWeighters["flat2050toPU30"] = edm.LumiReWeighting(puFile, puFile, "Flat20to50/pileup", "PU30/pileup")
-        self.newlumiWeighters["flat2050toPU25"] = edm.LumiReWeighting(puFile, puFile, "Flat20to50/pileup", "PU25/pileup")
+        #self.newlumiWeighters["flat2050toPU25"] = edm.LumiReWeighting(puFile, puFile, "Flat20to50/pileup", "PU25/pileup")
         self.newlumiWeighters["flat2050toPU20"] = edm.LumiReWeighting(puFile, puFile, "Flat20to50/pileup", "PU20/pileup")
         self.newlumiWeighters["flat2050toPU15"] = edm.LumiReWeighting(puFile, puFile, "Flat20to50/pileup", "PU15/pileup")
         self.newlumiWeighters["flat2050toPU10"] = edm.LumiReWeighting(puFile, puFile, "Flat20to50/pileup", "PU10/pileup")
+        #'''
         #self.newlumiWeighters["flat010toPU10"] = edm.LumiReWeighting(puFile, puFile, "Flat0to10/pileup", "PU10/pileup")
         #self.newlumiWeighters["flat010toPU1"] = edm.LumiReWeighting(puFile, puFile, "Flat0to10/pileup", "PU1/pileup")
         #self.newlumiWeighters["PU20toPU20"] = edm.LumiReWeighting(puFile, puFile, "PU20/pileup", "PU20/pileup")
@@ -41,7 +48,7 @@ class RateSimple(MNTriggerStudies.MNTriggerAna.ExampleProofReader.ExampleProofRe
 
         self.dists = {}
         for pu in self.newlumiWeighters:
-            dist = ROOT.TH1F(pu, "; jet seed threshold", 21, -0.5,20.5 )
+            dist = ROOT.TH1F(pu, "; jet seed threshold",  nbins, binL, binH )
             dist.Sumw2()
             self.dists[pu] = dist
             self.GetOutputList().Add(dist)
@@ -66,22 +73,83 @@ class RateSimple(MNTriggerStudies.MNTriggerAna.ExampleProofReader.ExampleProofRe
                 break
         del getBinCenter
 
+    def single(self, jets, etaMin = None, etaMax = None):
+        #bestJet = max(jets, key=lambda j: j.pt())
+        #maxThr = bestJet.pt()
+        maxThr = 0.
+        for i in xrange(jets.size()):
+            eta = abs(jets.at(i).eta())
+            if etaMin != None and eta < etaMin: continue
+            if etaMax != None and eta > etaMax: continue
+            pt = jets.at(i).pt()
+            if pt > maxThr:
+                maxThr = pt
+
+
+        return maxThr
+
+    def doubleJet(self, jets, fb=False):
+        maxThr = 0.
+        for i in xrange(jets.size()):
+            for j in xrange(i+1, jets.size()):
+                if fb:
+                    eta1 =  jets.at(i).eta()
+                    if abs(eta1) < 3.: continue
+                    eta2 =  jets.at(j).eta()
+                    if abs(eta2) < 3.: continue
+                    if eta1*eta2 > 0 : continue
+
+                contender = min(jets.at(i).pt(), jets.at(j).pt())
+                if contender > maxThr:
+                    maxThr = contender
+        return maxThr
+
+    def doubleJetAve(self, jets, probeCentral = False, etaProbeLimit = 2.7):
+        maxThr = 0.
+        ptmin = 10.
+        for i in xrange(jets.size()):
+            ptI = jets.at(i).pt()
+            if ptI < ptmin: continue
+
+            etaI = abs(jets.at(i).eta())
+            if etaI >  1.4: continue
+            for j in xrange(0, jets.size()):
+                if i == j: continue
+
+                ptJ = jets.at(j).pt()
+                if ptJ < ptmin: continue
+
+                etaJ = abs(jets.at(j).eta())
+                if not probeCentral:
+                    if etaJ <  etaProbeLimit: continue
+                else:
+                    if etaJ >  etaProbeLimit: continue
+                dphi = abs(ROOT.Math.VectorUtil.DeltaPhi(jets.at(j), jets.at(i)))
+                if dphi < 2.5: continue
+
+                ptAve = (ptI + ptJ)/2.
+                if ptAve > maxThr: maxThr = ptAve
+
+        return maxThr
 
     def analyze(self):
-        jets  = self.fChain.oldRedoneL1Jets
+        #jets  = self.fChain.oldRedoneL1Jets
+        #jets  = self.fChain.oldL1Jets
         #jets  = self.fChain.hltAK4PFJets
-        #jets = self.fChain.hltAK4PFJetsCorrected
+        jets = self.fChain.hltAK4PFJetsCorrected
+        #jets = self.fChain.ak4GenJets
 
+        maxThr = 0
         try:
-            #bestJet = max(jets, key=lambda j: j.pt())
-            #maxThr = bestJet.pt()
-            maxThr = 0.
-            for i in xrange(jets.size()):
-                for j in xrange(jets.size()):
-                    if i==j: continue
-                    contender = min(jets.at(i).pt(), jets.at(j).pt())
-                    if contender > maxThr:
-                        maxThr = contender
+            #maxThr = self.doubleJet(jets)
+            maxThr = self.doubleJet(jets, True)
+            #maxThr = self.doubleJetAve(jets)
+            #maxThr = self.doubleJetAve(jets, probeCentral=True)
+            #maxThr = self.doubleJetAve(jets, etaProbeLimit=3.139)
+            #maxThr = self.single(jets)
+            #maxThr = self.single(jets, etaMin=2.)
+            #maxThr = self.single(jets, etaMin=3.)
+            #maxThr = self.single(jets, etaMax=1.4)
         except ValueError:
             maxThr = 0
 
@@ -103,9 +171,13 @@ class RateSimple(MNTriggerStudies.MNTriggerAna.ExampleProofReader.ExampleProofRe
         lhcFreq = 4E7 # 40 MHz
         totalBunches = 3564.
         #  (usually 2662 for 25ns bunch spacing, 1331 for 50ns bunch spacing) 
-        filledBunches = 40.
+        #filledBunches = 40.
+        filledBunches = 1331
 
-        PU=0.01
+        #PU=0.01
+        #PU=0.4
+        #PU=0.4
+        PU=1
 
         rateScalingFactor = lhcFreq*float(filledBunches)/float(totalBunches)*PU
 
@@ -130,6 +202,18 @@ class RateSimple(MNTriggerStudies.MNTriggerAna.ExampleProofReader.ExampleProofRe
                 print puVal, eff.GetBinCenter(15), eff.GetBinContent(15),  eff.GetBinError(15)
                 res[puVal] = eff.GetBinContent(15)
 
+                todoPT = [16, 36, 52, 68, 128, 176]
+                for pt in todoPT:
+                    bin = rate.FindBin(pt-1)
+                    val = rate.GetBinContent(bin)
+                    err = rate.GetBinError(bin)
+                    rel = "-"
+                    if val > 0:
+                        rel = err/val
+                    print "PU", puVal, "L1singleJet"+str(pt), val, err, rel
+
+
+
         for p in sorted(res.keys()):
             print "Prob at PU =",p, res[p]
 '''
@@ -150,9 +234,11 @@ if __name__ == "__main__":
     AutoLibraryLoader.enable()
 
     #sampleList = ["MinBias_TuneZ2star_13TeV_pythia6_162"]
-    sampleList = ["Neutrino_Pt-2to20_gun"]
-    maxFilesMC = None
-    nWorkers = 4
+    #sampleList = ["QCD_Pt-15to3000_Tune4C_Flat_13TeV_pythia8"]
+    #sampleList = ["Neutrino_Pt-2to20_gun"]
+    sampleList = ["Neutrino_Pt-2to20_gun_162"]
+    maxFilesMC = 40
+    nWorkers = 10
     slaveParams = {}
 
 
