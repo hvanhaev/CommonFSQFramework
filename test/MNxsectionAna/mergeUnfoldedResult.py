b346fd83884b2d6965c42d842d66c85fe0fd366b
Tomasz <fruboes@cern.ch>
2015-03-13 11:10:22 +0100

    Final plot

--- test/MNxsectionAna/mergeUnfoldedResult.py
+++ test/MNxsectionAna/mergeUnfoldedResult.py
@@ -5,7 +5,14 @@ ROOT.gROOT.SetBatch(True)
 
 from HistosHelper import getHistos
 
+from MNTriggerStudies.MNTriggerAna.DrawPlots import DrawPlots
+
+import  MNTriggerStudies.MNTriggerAna.Style
+
+from mnDraw import DrawMNPlots 
+
 def main():
+    MNTriggerStudies.MNTriggerAna.Style.setTDRStyle()
 
     lumiUncertainty = 0.04
     herwigIn="~/tmp/mnxsHistos_unfolded_herwigOnData.root"
@@ -64,9 +71,9 @@ def main():
     finalSet["merged"] = {}
     for t in finalSet["_jet15"]:
         newName = t.replace("_jet15", "_jet15andDJ15FB")
-        finalHisto = finalSet["_jet15"][t].Clone()
+        finalHisto = finalSet["_jet15"][t].Clone(newName)
         finalHisto.Add(finalSet["_dj15fb"][t.replace("_jet15", "_dj15fb")].Clone())
-        finalSet["merged"][t] = finalHisto
+        finalSet["merged"][newName] = finalHisto
 
     # save all to file
     ofile = ROOT.TFile(ofileName, "RECREATE")
@@ -74,6 +81,45 @@ def main():
         odir = ofile.mkdir(dirName)
         for h in finalSet[dirName]:
             odir.WriteTObject(finalSet[dirName][h])
+
+
+    # make final plot, including uncertainty band
+    central = [ finalSet["merged"][hName] for hName in finalSet["merged"].keys() if "_central_" in hName ]
+    if len(central) != 1:
+        raise Exception("Error: more than one central histo found")
+    central = central[0]
+
+    uncert  = [finalSet["merged"][hName] for hName in finalSet["merged"].keys() if "_central_" not in hName ]
+
+    uncResult= DrawPlots.getUncertaintyBand(uncert, central)
+    unc = uncResult["band"]
+    maxima = []
+    maxima.append(uncResult["max"])
+    maxima.append(unc.GetMaximum())
+    maxima.append(central.GetMaximum())
+
+    c = ROOT.TCanvas()
+    c.SetTopMargin(0.1)
+    c.SetRightMargin(0.07)
+    central.SetMaximum(max(maxima)*1.05)
+    unc.SetFillColor(17);
+    central.Draw()
+    central.GetXaxis().SetTitle("#Delta#eta")
+    central.GetYaxis().SetTitle("#sigma [pb]")
+    central.GetYaxis().SetTitleOffset(1.8)
+    unc.Draw("2SAME")
+    DrawMNPlots.banner()
+    c.Print("~/tmp/mergedUnfolded.png")
+    c.SetLogy()
+    c.Print("~/tmp/mergedUnfolded_log.png")
+
+
+
+
+
+
+
+    #central = filter(lambda  finalSet["merged"])
 
 if __name__ == "__main__":
     main()
