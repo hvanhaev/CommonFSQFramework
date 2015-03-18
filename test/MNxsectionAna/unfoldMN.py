03514b885c964cae33f87224c507f7aea71b64d0
Tomasz <fruboes@cern.ch>
2015-03-18 14:35:32 +0100

    Unfolding experimentation

--- test/MNxsectionAna/unfoldMN.py
+++ test/MNxsectionAna/unfoldMN.py
@@ -41,18 +41,22 @@ def unfold(action):
         categories["_dj15fb"] = ["METFwd-Run2010B-Apr21ReReco-v1", "JetMETTau-Run2010A-Apr21ReReco-v1", "JetMET-Run2010A-Apr21ReReco-v1"]
     elif action ==  "pythiaOnHerwig":
         baseMC = "QCD_Pt-15to3000_TuneZ2star_Flat_HFshowerLibrary_7TeV_pythia6"
+        otherMC =  "QCD_Pt-15to1000_TuneEE3C_Flat_7TeV_herwigpp"
         categories["_jet15"] = ["QCD_Pt-15to1000_TuneEE3C_Flat_7TeV_herwigpp"]
         categories["_dj15fb"] = ["QCD_Pt-15to1000_TuneEE3C_Flat_7TeV_herwigpp"]
     elif action ==  "herwigOnPythia":
         baseMC = "QCD_Pt-15to1000_TuneEE3C_Flat_7TeV_herwigpp"
+        otherMC = "QCD_Pt-15to3000_TuneZ2star_Flat_HFshowerLibrary_7TeV_pythia6"
         categories["_jet15"] = ["QCD_Pt-15to3000_TuneZ2star_Flat_HFshowerLibrary_7TeV_pythia6"]
         categories["_dj15fb"] = ["QCD_Pt-15to3000_TuneZ2star_Flat_HFshowerLibrary_7TeV_pythia6"]
     elif action ==  "herwigOnHerwig":
         baseMC = "QCD_Pt-15to1000_TuneEE3C_Flat_7TeV_herwigpp"
+        otherMC =  "QCD_Pt-15to1000_TuneEE3C_Flat_7TeV_herwigpp"
         categories["_jet15"] = ["QCD_Pt-15to1000_TuneEE3C_Flat_7TeV_herwigpp"]
         categories["_dj15fb"] = ["QCD_Pt-15to1000_TuneEE3C_Flat_7TeV_herwigpp"]
     elif action ==  "pythiaOnPythia":
         baseMC = "QCD_Pt-15to3000_TuneZ2star_Flat_HFshowerLibrary_7TeV_pythia6"
+        otherMC = "QCD_Pt-15to3000_TuneZ2star_Flat_HFshowerLibrary_7TeV_pythia6"
         categories["_jet15"] = ["QCD_Pt-15to3000_TuneZ2star_Flat_HFshowerLibrary_7TeV_pythia6"]
         categories["_dj15fb"] = ["QCD_Pt-15to3000_TuneZ2star_Flat_HFshowerLibrary_7TeV_pythia6"]
 
@@ -116,8 +120,6 @@ def unfold(action):
             rawName = "xsunfolded_" + variation+ c
             sys.stdout.flush()
 
-            print type(histos[baseMC][r])
-
             if alaGri:
                 acc = []
                 print "Note: subs.bkg", r
@@ -163,9 +165,87 @@ def unfold(action):
                 
 
 
+            if False: # second approach to alaGri
+                # histos[baseMC][r] - response object
+                # histo - detector level distribution
+                #histo.Scale(0.5)
+                responseWithBrokenFakesNorm = histos[baseMC][r]
+                #   RooUnfoldResponse(const TH1* measured, const TH1* truth, const TH2* response
+                meas  =  responseWithBrokenFakesNorm.Hmeasured().Clone()
+                truth =  responseWithBrokenFakesNorm.Htruth().Clone()
+                fake =  responseWithBrokenFakesNorm.Hfakes().Clone()
+                fakeScaled =  responseWithBrokenFakesNorm.Hfakes().Clone()
+                resp= responseWithBrokenFakesNorm.Hresponse().Clone()
+                #scalingFactor = histo.Integral()/histos[baseMC][centralHistoName].Integral()
+                scalingFactor = histo.Integral()/meas.Integral()
 
-            # histos[baseMC][r] - response object
-            # histo - detector level distribution
+                # Note: simple call to h.Scale() is not enough
+                def scale(h, s):
+                    #h.Scale(s)
+                    #return
+                    for i in xrange(0, h.GetNbinsX()+1):
+                        val = h.GetBinContent(i)*s
+                        err = h.GetBinError(i)*s
+                        h.SetBinContent(i, val)
+                        h.SetBinError(i, err)
+
+                def scale2d(h, s):
+                    for i in xrange(0, h.GetNbinsX()+1):
+                        for j in xrange(0, h.GetNbinsY()+1):
+                            val = h.GetBinContent(i,j)*s
+                            err = h.GetBinError(i, j)*s
+                            h.SetBinContent(i, j, val)
+                            h.SetBinError(i, j, err)
+
+                for i in xrange(0, h.GetNbinsX()+1):
+                    denom = meas.GetBinContent(i)
+                    if denom == 0: continue
+                    nom = fake.GetBinContent(i)
+                    if nom > denom:
+                        print "AAAA", nom, denom
+                    factor = 1.-nom/denom
+                    val = histo.GetBinContent(i)*factor
+                    err = histo.GetBinError(i)*factor
+                    histo.SetBinContent(i, val)
+                    histo.SetBinError(i, err)
+
+
+                #meas.Add(fake,-1)
+                #print "AAAAA", r, fake.Integral()
+                #scale(fakeScaled, scalingFactor)
+                #histo.Add(fakeScaled, -1)
+                #meas.Add(fakeScaled)
+                #scale(meas, scalingFactor)
+                #scale(truth, scalingFactor)
+                #scale2d(resp, scalingFactor)
+                meas.Reset()
+                newResponseTmp = ROOT.RooUnfoldResponse(meas, truth, resp)
+
+                measNew = newResponseTmp.Hfakes()
+                scale(measNew, scalingFactor)
+                scale(truth, scalingFactor)
+                scale2d(resp, scalingFactor)
+                newResponse = ROOT.RooUnfoldResponse(measNew, truth, resp)
+
+                #newResponse = ROOT.RooUnfoldResponse(meas, truth, resp)
+                #newResponse.Hfakes().Scale(0)
+                #print "AAA",r, newResponse.Hfakes().Integral()
+
+                '''
+                # 
+                newFakes = newResponse.Hfakes() 
+                scalingFactor = histo.Integral()/meas.Integral()
+                #scalingFactor = meas.Integral()/histo.Integral()
+                newFakes.Scale(scalingFactor)
+                deltaFakes = newFakes.Clone()
+                deltaFakes.Add(responseWithBrokenFakesNorm.Hfakes(), -1)
+                newMeas = newResponse.Hmeasured()
+                newMeas.Add(deltaFakes, -1)
+                '''
+
+                #unfold = ROOT.RooUnfoldBayes(histos[baseMC][r], histo, 10)
+                unfold = ROOT.RooUnfoldBayes(newResponse, histo, 10)
+
             unfold = ROOT.RooUnfoldBayes(histos[baseMC][r], histo, 10)
             hReco= unfold.Hreco()
             if hReco.GetNbinsX() != histo.GetNbinsX():
@@ -217,6 +297,7 @@ def compareMCGentoMCUnfolded(action):
         unfoldedHistoName = t+"/xsunfolded_central"+t
         unfoldedHisto = fileWithUnfoldedPlots.Get(unfoldedHistoName)
         #print unfoldedHistoName, type(unfoldedHisto), unfoldedHisto.ClassName()
+        #genHisto.Scale(0.5)
         genHisto.Draw()
         genHisto.GetXaxis().SetTitle(DrawMNPlots.xLabels()["xs"])
         genHisto.GetYaxis().SetTitleOffset(1.8)
@@ -230,20 +311,13 @@ def compareMCGentoMCUnfolded(action):
 
         c.Print("~/tmp/MConMCunfoldingTest_"+action+t+".png")
 
-
-
-
-
-
-
-
 def main():
     MNTriggerStudies.MNTriggerAna.Style.setTDRStyle()
     possibleActions = getPossibleActions()
     global alaGri
     alaGri = False
 
-    #possibleActions = ["pythiaOnPythia",  "herwigOnPythia", "pythiaOnHerwig", "herwigOnHerwig"]
+    possibleActions = ["pythiaOnPythia",  "herwigOnPythia", "pythiaOnHerwig", "herwigOnHerwig"]
     for action in possibleActions:
         unfold(action)
         compareMCGentoMCUnfolded(action)
