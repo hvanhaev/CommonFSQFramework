ebae77a108696e02e95c5bc6b2f26d82f82feb31
Tomasz <fruboes@cern.ch>
2015-04-01 09:58:46 +0200

    Fix paths

--- Core/test/MNxsectionAna/do.py
+++ Core/test/MNxsectionAna/do.py
@@ -8,13 +8,14 @@ import fnmatch
 #todoSteps = ["proof", "hadd", "draw", "unfold", "merge"]
 #todoCat = ["InclusiveBasic", "InclusiveAsym", "InclusiveWindow", "MNBasic", "MNAsym", "MNWindow"]
 #todoCat = ["InclusiveBasic", "InclusiveAsym", "InclusiveWindow", "MNBasic", "MNAsym", "MNWindow", "FWD11_002"]
-todoCat = ["InclusiveBasic"]
+#todoCat = ["InclusiveBasic"]
+todoCat = ["MNAsym"]
 #todoCat = ["FWD11_002"]
 #todoCat = ["InclusiveBasic"]
 todoSteps = []
-todoSteps.append("proof")
+#todoSteps.append("proof")
 #todoSteps.append("hadd")
-#todoSteps.append("draw")
+todoSteps.append("draw")
 #todoSteps.append("unfold")
 #todoSteps.append("merge")
 
@@ -24,7 +25,7 @@ for cat in todoCat:
             os.system("./MNxsAnalyzerClean.py -v {}".format(cat))
 
         elif step == "draw":
-            command="../mnDraw.py -i plotsMNxs_XXS.root -o ~/tmp/MNXS_XXS_detectorLevel_XXM/ -v XXM"
+            command="./mnDraw.py -i plotsMNxs_XXS.root -o ~/tmp/MNXS_XXS_detectorLevel_XXM/ -v XXM"
             os.system(command.replace("XXS", cat).replace("XXM", "herwig"))
             os.system(command.replace("XXS", cat).replace("XXM", "pythia"))
         elif step == "hadd":
@@ -34,12 +35,12 @@ for cat in todoCat:
             for file in os.listdir('.'):
                 if fnmatch.fnmatch(file, 'plotsMNxs_{}_*.root'.format(cat)):
                     infiles.append(file)
-            os.system("../hadd.py " + target + " " + " ".join(infiles) )
+            os.system("./hadd.py " + target + " " + " ".join(infiles) )
         elif step == "unfold":
-            os.system("../unfoldMN.py -v {}".format(cat))
+            os.system("./unfoldMN.py -v {}".format(cat))
         elif step == "merge":
             if cat == "FWD11_002":
-                os.system("../mergeUnfoldedResult.py -v {} -n xs  -b -s 5.6".format(cat))
+                os.system("./mergeUnfoldedResult.py -v {} -n xs  -b -s 5.6".format(cat))
             else:
-                os.system("../mergeUnfoldedResult.py -v {} -n xs".format(cat))
-                os.system("../mergeUnfoldedResult.py -v {} -n area".format(cat))
+                os.system("./mergeUnfoldedResult.py -v {} -n xs".format(cat))
+                os.system("./mergeUnfoldedResult.py -v {} -n area".format(cat))
