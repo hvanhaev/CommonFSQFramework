66c533fd41c9f0eb4673b6b971ab4e1dcf280e04
Tomasz <fruboes@cern.ch>
2015-04-08 18:10:59 +0200

    inici

---
+++ Core/plugins/CFFTreeProducer.cc
@@ -0,0 +1,157 @@
+// -*- C++ -*-
+//
+// Package:    CFFTreeProducer
+// Class:      CFFTreeProducer
+// 
+/**\class CFFTreeProducer CFFTreeProducer.cc MNTriggerStudies/CFFTreeProducer/plugins/CFFTreeProducer.cc
+*/
+
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "TTree.h"
+
+#include "DataFormats/PatCandidates/interface/Jet.h"
+#include <DataFormats/PatCandidates/interface/TriggerEvent.h>
+
+#include "CommonFSQFramework/Core/interface/EventIdData.h"
+#include "CommonFSQFramework/Core/interface/GenTrackView.h"
+#include "CommonFSQFramework/Core/interface/RecoTrackView.h"
+#include "CommonFSQFramework/Core/interface/VerticesView.h"
+
+//
+// class declaration
+//
+
+class CFFTreeProducer : public edm::EDAnalyzer {
+   public:
+      explicit CFFTreeProducer(const edm::ParameterSet&);
+      ~CFFTreeProducer();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+
+   private:
+      virtual void beginJob();
+      virtual void analyze(const edm::Event&, const edm::EventSetup&);
+      virtual void endJob();
+
+      TTree *m_tree;
+      std::vector<EventViewBase *> m_views;
+
+      //virtual void beginRun(edm::Run const&, edm::EventSetup const&) override;
+      //virtual void endRun(edm::Run const&, edm::EventSetup const&) override;
+      //virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+      //virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+
+      // ----------member data ---------------------------
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+CFFTreeProducer::CFFTreeProducer(const edm::ParameterSet& iConfig)
+
+{
+    edm::Service<TFileService> tFileService;
+    m_tree = tFileService->make<TTree>("data", "data");
+    m_views.push_back(new EventIdData(iConfig.getParameter< edm::ParameterSet >("EventData"), m_tree));
+    m_views.push_back(new GenTrackView(iConfig.getParameter< edm::ParameterSet >("GenTrackView"), m_tree));
+    m_views.push_back(new RecoTrackView(iConfig.getParameter< edm::ParameterSet >("RecoTrackView"), m_tree));
+    m_views.push_back(new VerticesView(iConfig.getParameter< edm::ParameterSet >("VerticesView"), m_tree));
+}
+
+CFFTreeProducer::~CFFTreeProducer() {}
+
+void
+CFFTreeProducer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup){
+
+    using namespace edm;
+    for (unsigned int i = 0; i < m_views.size(); ++i){
+        m_views[i]->fill(iEvent, iSetup);
+    }
+    m_tree->Fill();
+
+}
+
+
+// ------------ method called once each job just before starting event loop  ------------
+void 
+CFFTreeProducer::beginJob()
+{
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void 
+CFFTreeProducer::endJob() 
+{
+}
+
+// ------------ method called when starting to processes a run  ------------
+/*
+void 
+CFFTreeProducer::beginRun(edm::Run const&, edm::EventSetup const&)
+{
+}
+*/
+
+// ------------ method called when ending the processing of a run  ------------
+/*
+void 
+CFFTreeProducer::endRun(edm::Run const&, edm::EventSetup const&)
+{
+}
+*/
+
+// ------------ method called when starting to processes a luminosity block  ------------
+/*
+void 
+CFFTreeProducer::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{
+}
+*/
+
+// ------------ method called when ending the processing of a luminosity block  ------------
+/*
+void 
+CFFTreeProducer::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{
+}
+*/
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void
+CFFTreeProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+
+
+
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(CFFTreeProducer);
