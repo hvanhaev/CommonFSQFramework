353eab03d7565de98249074e5ea9d74c7faf634c
Hans Van Haevermaet <hans.van.haevermaet@cern.ch>
2015-04-15 16:25:48 +0200

    bugfix

--- Core/env/do_RunIIWinter15GS_14042015.sh
+++ Core/env/do_RunIIWinter15GS_14042015.sh
@@ -1,2 +1,2 @@
-export SmallXAnaVersion="CommonFSQFramework.Core.samples.Samples_RunIIWinter15GS_14042015.py"
+export SmallXAnaVersion="CommonFSQFramework.Core.samples.Samples_RunIIWinter15GS_14042015"
 
