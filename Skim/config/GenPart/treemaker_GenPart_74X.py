6ee2122c7d71f44036675f20b373f207e4a795a1
Hans Van Haevermaet <hans.van.haevermaet@cern.ch>
2015-04-16 16:59:37 +0200

    first mv and rename of directory structure for skimming

---
+++ Skim/config/GenPart/treemaker_GenPart_74X.py
@@ -0,0 +1,35 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Treemaker")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1000))
+
+process.MessageLogger.cerr.FwkReport.reportEvery = 10000
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+
+# Source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:00E5660A-47C7-E411-9E5B-0025905A48F0.root')
+)
+
+# Geometry and Detector Conditions
+process.load("Configuration.Geometry.GeometryRecoDB_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc')
+process.load("Configuration.StandardSequences.MagneticField_cff")
+
+# Here starts the CFF specific part
+import CommonFSQFramework.Core.customizePAT
+process = CommonFSQFramework.Core.customizePAT.customize(process)
+
+# GT customization
+process = CommonFSQFramework.Core.customizePAT.customizeGT(process)
+
+# define treeproducer
+process.GenPartTree = cms.EDAnalyzer("CFFTreeProducer")
+import CommonFSQFramework.Core.GenPartViewsConfigs
+process.GenPartTree._Parameterizable__setParameters(CommonFSQFramework.Core.GenPartViewsConfigs.get(["GenPartView"]))
+process = CommonFSQFramework.Core.customizePAT.addTreeProducer(process, process.GenPartTree)
