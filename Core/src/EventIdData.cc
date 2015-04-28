9dbe53757dbcf8c6167924ad1f009f3b66a0963f
Hans Van Haevermaet <hans.van.haevermaet@cern.ch>
2015-04-28 15:38:00 +0200

    add calculation of Xi variables + processID

--- Core/src/EventIdData.cc
+++ Core/src/EventIdData.cc
@@ -1,6 +1,9 @@
 #include "CommonFSQFramework/Core/interface/EventIdData.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 #include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
+#include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
 #include "SimDataFormats/PileupSummaryInfo/interface/PileupSummaryInfo.h"
+#include "TLorentzVector.h"
 
 EventIdData::EventIdData(const edm::ParameterSet& iConfig, TTree * tree):
 EventViewBase(iConfig, tree)
@@ -13,6 +16,12 @@ EventViewBase(iConfig, tree)
 
     registerFloat("alphaQCD", tree);
     registerFloat("qScale", tree);
+    
+    registerInt("processID", tree);
+    registerFloat("Xix", tree);
+    registerFloat("Xiy", tree);
+    registerFloat("XiSD", tree);
+    registerFloat("XiDD", tree);
     
 
     registerFloat("puTrueNumInteractions", tree);
@@ -37,6 +46,102 @@ void EventIdData::fillSpecific(const edm::Event& iEvent, const edm::EventSetup& 
     setF("genWeight", hGW->weight());
     setF("alphaQCD", hGW->alphaQCD());
     setF("qScale", hGW->qScale());
+    setI("processID",hGW->signalProcessID());
+    
+    // calculate the Xi's of the event
+    double xix = 100;
+    double xiy = 100;
+    double xisd = 100;
+    double xidd = 10e10;
+    
+    // get the HepMC information
+    edm::Handle<edm::HepMCProduct> hepmc;
+    iEvent.getByLabel("generator", hepmc);
+    
+    std::vector<TLorentzVector> myTempParticles;
+    std::vector<TLorentzVector> myRapiditySortedParticles;
+    
+    // copy only final state particles from the genParticles collection
+    for(HepMC::GenEvent::particle_const_iterator p = hepmc->GetEvent()->particles_begin(); p != hepmc->GetEvent()->particles_end(); ++p) {
+    	if ((*p)->status() != 1) continue;
+	TLorentzVector vp;
+	vp.SetPxPyPzE((*p)->momentum().px(),(*p)->momentum().py(),(*p)->momentum().pz(),(*p)->momentum().e());
+    	myTempParticles.push_back(vp);
+    }
+    		
+    // sort genParticles in y, from y_min to y_max
+    while (myTempParticles.size() != 0) {
+    	double min_y = 10000;
+	int min_y_pos = -1;
+	for (unsigned int ipart = 0;ipart<myTempParticles.size();ipart++) {
+		if (myTempParticles[ipart].Rapidity() < min_y) {
+			min_y = myTempParticles[ipart].Rapidity();
+			min_y_pos = ipart;
+		}
+	}
+	myRapiditySortedParticles.push_back(myTempParticles[min_y_pos]);
+	myTempParticles.erase(myTempParticles.begin()+min_y_pos);
+    }
+			
+    // find deltaymax
+    double deltaymax = 0;
+    int deltaymax_pos = -1;
+    for (unsigned int ipart=0;ipart<myRapiditySortedParticles.size()-1;ipart++) {
+    	double deltay = myRapiditySortedParticles[ipart+1].Rapidity() - myRapiditySortedParticles[ipart].Rapidity();
+	if (deltay > deltaymax) {
+		deltaymax = deltay;
+		deltaymax_pos = ipart;
+	}
+    }
+			
+    // calculate Mx2 and My2
+    long double XEtot = 0;
+    long double XPxtot = 0;
+    long double XPytot = 0;
+    long double XPztot = 0;
+    long double YEtot = 0;
+    long double YPxtot = 0;
+    long double YPytot = 0;
+    long double YPztot = 0;
+			
+    for (int ipart=0;ipart<=deltaymax_pos;ipart++) {
+    	XEtot += myRapiditySortedParticles[ipart].E();
+	XPxtot += myRapiditySortedParticles[ipart].Px();
+	XPytot += myRapiditySortedParticles[ipart].Py();
+	XPztot += myRapiditySortedParticles[ipart].Pz();
+    }
+    long double Mx2 = -1.;
+    Mx2 = XEtot*XEtot - XPxtot*XPxtot - XPytot*XPytot - XPztot*XPztot;
+			
+    for (unsigned int ipart=deltaymax_pos+1;ipart<myRapiditySortedParticles.size();ipart++) {
+    	YEtot += myRapiditySortedParticles[ipart].E();
+	YPxtot += myRapiditySortedParticles[ipart].Px();
+	YPytot += myRapiditySortedParticles[ipart].Py();
+	YPztot += myRapiditySortedParticles[ipart].Pz();
+    }
+    long double My2 = -1.;
+    My2 = YEtot*YEtot - YPxtot*YPxtot - YPytot*YPytot - YPztot*YPztot;
+			
+    // get the centre-of-mass energy from the event
+    double cmenergy = 0;
+    cmenergy = std::abs(hepmc->GetEvent()->beam_particles().first->momentum().pz()) + std::abs(hepmc->GetEvent()->beam_particles().second->momentum().pz());
+    			
+    // calculate xix and xiy
+    xix = Mx2/(cmenergy*cmenergy);
+    xiy = My2/(cmenergy*cmenergy);
+			
+    // xisd and xidd
+    xisd = std::max(xix,xiy);
+    xidd = xix*xiy*cmenergy*cmenergy/(0.938*0.938);
+    
+    // fill tree
+    setF("Xix",xix);
+    setF("Xiy",xiy);
+    setF("XiSD",xisd);
+    setF("XiDD",xidd);
+    
+    
+    // pile-up stuff
 
     try {
     	edm::Handle< std::vector<PileupSummaryInfo> > hPU;
