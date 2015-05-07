044e6cea5555cdd042d142462fbbe230b0115007
Hans Van Haevermaet <hans.van.haevermaet@cern.ch>
2015-05-07 17:27:27 +0200

    finalize skim

--- Skim/python/Samples_RunIIWinter15GS_07052015.py
+++ Skim/python/Samples_RunIIWinter15GS_07052015.py
@@ -14,9 +14,12 @@ sam["MinBias_TuneCUETP8M1_13TeV-pythia8"]["GT"]='MCRUN2_71_V0::All'
 sam["MinBias_TuneCUETP8M1_13TeV-pythia8"]["name"]='MinBias_TuneCUETP8M1_13TeV-pythia8'
 sam["MinBias_TuneCUETP8M1_13TeV-pythia8"]["isData"]=False
 sam["MinBias_TuneCUETP8M1_13TeV-pythia8"]["numEvents"]=999330
+sam["MinBias_TuneCUETP8M1_13TeV-pythia8"]["pathSE"]='srm://maite.iihe.ac.be:8443/srm/managerv2?SFN=/pnfs/iihe/cms/store/user/hvanhaev/MinBias_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS_07052015_MinBias_TuneCUETP8M1_13TeV-pythia8/150507_124719/0000/'
+sam["MinBias_TuneCUETP8M1_13TeV-pythia8"]["pathTrees"]='/XXXTMFTTree/store/user/hvanhaev/MinBias_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS_07052015_MinBias_TuneCUETP8M1_13TeV-pythia8/150507_124719/0000//'
 sam["MinBias_TuneCUETP8M1_13TeV-pythia8"]["json"]=''
 sam["MinBias_TuneCUETP8M1_13TeV-pythia8"]["lumiMinBias"]=1.2743565285698254e-05
 sam["MinBias_TuneCUETP8M1_13TeV-pythia8"]["XS"]=78418400000.0
+sam["MinBias_TuneCUETP8M1_13TeV-pythia8"]["pathPAT"]='/XXXTMFPAT/store/user/hvanhaev/MinBias_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS_07052015_MinBias_TuneCUETP8M1_13TeV-pythia8/150507_124719/0000//'
 sam["MinBias_TuneCUETP8M1_13TeV-pythia8"]["DS"]='/MinBias_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS-castor_MCRUN2_71_V0-v1/GEN-SIM'
 
 sam["MinBias_TuneEE5C_13TeV-herwigpp"]={}
@@ -25,9 +28,12 @@ sam["MinBias_TuneEE5C_13TeV-herwigpp"]["GT"]='MCRUN2_71_V0::All'
 sam["MinBias_TuneEE5C_13TeV-herwigpp"]["name"]='MinBias_TuneEE5C_13TeV-herwigpp'
 sam["MinBias_TuneEE5C_13TeV-herwigpp"]["isData"]=False
 sam["MinBias_TuneEE5C_13TeV-herwigpp"]["numEvents"]=1000000
+sam["MinBias_TuneEE5C_13TeV-herwigpp"]["pathSE"]='srm://maite.iihe.ac.be:8443/srm/managerv2?SFN=/pnfs/iihe/cms/store/user/hvanhaev/MinBias_TuneEE5C_13TeV-herwigpp/RunIIWinter15GS_07052015_MinBias_TuneEE5C_13TeV-herwigpp/150507_124632/0000/'
+sam["MinBias_TuneEE5C_13TeV-herwigpp"]["pathTrees"]='/XXXTMFTTree/store/user/hvanhaev/MinBias_TuneEE5C_13TeV-herwigpp/RunIIWinter15GS_07052015_MinBias_TuneEE5C_13TeV-herwigpp/150507_124632/0000//'
 sam["MinBias_TuneEE5C_13TeV-herwigpp"]["json"]=''
 sam["MinBias_TuneEE5C_13TeV-herwigpp"]["lumiMinBias"]=2.7427317608337905e-05
 sam["MinBias_TuneEE5C_13TeV-herwigpp"]["XS"]=36460000000.0
+sam["MinBias_TuneEE5C_13TeV-herwigpp"]["pathPAT"]='/XXXTMFPAT/store/user/hvanhaev/MinBias_TuneEE5C_13TeV-herwigpp/RunIIWinter15GS_07052015_MinBias_TuneEE5C_13TeV-herwigpp/150507_124632/0000//'
 sam["MinBias_TuneEE5C_13TeV-herwigpp"]["DS"]='/MinBias_TuneEE5C_13TeV-herwigpp/RunIIWinter15GS-castor_MCRUN2_71_V0-v1/GEN-SIM'
 
 sam["MinBias_TuneMonash13_13TeV-pythia8"]={}
@@ -36,9 +42,12 @@ sam["MinBias_TuneMonash13_13TeV-pythia8"]["GT"]='MCRUN2_71_V0::All'
 sam["MinBias_TuneMonash13_13TeV-pythia8"]["name"]='MinBias_TuneMonash13_13TeV-pythia8'
 sam["MinBias_TuneMonash13_13TeV-pythia8"]["isData"]=False
 sam["MinBias_TuneMonash13_13TeV-pythia8"]["numEvents"]=998368
+sam["MinBias_TuneMonash13_13TeV-pythia8"]["pathSE"]='srm://maite.iihe.ac.be:8443/srm/managerv2?SFN=/pnfs/iihe/cms/store/user/hvanhaev/MinBias_TuneMonash13_13TeV-pythia8/RunIIWinter15GS_07052015_MinBias_TuneMonash13_13TeV-pythia8/150507_124710/0000/'
+sam["MinBias_TuneMonash13_13TeV-pythia8"]["pathTrees"]='/XXXTMFTTree/store/user/hvanhaev/MinBias_TuneMonash13_13TeV-pythia8/RunIIWinter15GS_07052015_MinBias_TuneMonash13_13TeV-pythia8/150507_124710/0000//'
 sam["MinBias_TuneMonash13_13TeV-pythia8"]["json"]=''
 sam["MinBias_TuneMonash13_13TeV-pythia8"]["lumiMinBias"]=1.273129775664895e-05
 sam["MinBias_TuneMonash13_13TeV-pythia8"]["XS"]=78418400000.0
+sam["MinBias_TuneMonash13_13TeV-pythia8"]["pathPAT"]='/XXXTMFPAT/store/user/hvanhaev/MinBias_TuneMonash13_13TeV-pythia8/RunIIWinter15GS_07052015_MinBias_TuneMonash13_13TeV-pythia8/150507_124710/0000//'
 sam["MinBias_TuneMonash13_13TeV-pythia8"]["DS"]='/MinBias_TuneMonash13_13TeV-pythia8/RunIIWinter15GS-castor_MCRUN2_71_V0-v1/GEN-SIM'
 
 sam["MinBias_TuneZ2star_13TeV-pythia6"]={}
@@ -47,9 +56,12 @@ sam["MinBias_TuneZ2star_13TeV-pythia6"]["GT"]='MCRUN2_71_V0::All'
 sam["MinBias_TuneZ2star_13TeV-pythia6"]["name"]='MinBias_TuneZ2star_13TeV-pythia6'
 sam["MinBias_TuneZ2star_13TeV-pythia6"]["isData"]=False
 sam["MinBias_TuneZ2star_13TeV-pythia6"]["numEvents"]=998098
+sam["MinBias_TuneZ2star_13TeV-pythia6"]["pathSE"]='srm://maite.iihe.ac.be:8443/srm/managerv2?SFN=/pnfs/iihe/cms/store/user/hvanhaev/MinBias_TuneZ2star_13TeV-pythia6/RunIIWinter15GS_07052015_MinBias_TuneZ2star_13TeV-pythia6/150507_124656/0000/'
+sam["MinBias_TuneZ2star_13TeV-pythia6"]["pathTrees"]='/XXXTMFTTree/store/user/hvanhaev/MinBias_TuneZ2star_13TeV-pythia6/RunIIWinter15GS_07052015_MinBias_TuneZ2star_13TeV-pythia6/150507_124656/0000//'
 sam["MinBias_TuneZ2star_13TeV-pythia6"]["json"]=''
 sam["MinBias_TuneZ2star_13TeV-pythia6"]["lumiMinBias"]=1.275361615129057e-05
 sam["MinBias_TuneZ2star_13TeV-pythia6"]["XS"]=78260000000.0
+sam["MinBias_TuneZ2star_13TeV-pythia6"]["pathPAT"]='/XXXTMFPAT/store/user/hvanhaev/MinBias_TuneZ2star_13TeV-pythia6/RunIIWinter15GS_07052015_MinBias_TuneZ2star_13TeV-pythia6/150507_124656/0000//'
 sam["MinBias_TuneZ2star_13TeV-pythia6"]["DS"]='/MinBias_TuneZ2star_13TeV-pythia6/RunIIWinter15GS-castor_MCRUN2_71_V0-v1/GEN-SIM'
 
 sam["ReggeGribovPartonMC_13TeV-EPOS"]={}
@@ -58,9 +70,12 @@ sam["ReggeGribovPartonMC_13TeV-EPOS"]["GT"]='MCRUN2_71_V0::All'
 sam["ReggeGribovPartonMC_13TeV-EPOS"]["name"]='ReggeGribovPartonMC_13TeV-EPOS'
 sam["ReggeGribovPartonMC_13TeV-EPOS"]["isData"]=False
 sam["ReggeGribovPartonMC_13TeV-EPOS"]["numEvents"]=998312
+sam["ReggeGribovPartonMC_13TeV-EPOS"]["pathSE"]='srm://maite.iihe.ac.be:8443/srm/managerv2?SFN=/pnfs/iihe/cms/store/user/hvanhaev/ReggeGribovPartonMC_13TeV-EPOS/RunIIWinter15GS_07052015_ReggeGribovPartonMC_13TeV-EPOS/150507_124643/0000/'
+sam["ReggeGribovPartonMC_13TeV-EPOS"]["pathTrees"]='/XXXTMFTTree/store/user/hvanhaev/ReggeGribovPartonMC_13TeV-EPOS/RunIIWinter15GS_07052015_ReggeGribovPartonMC_13TeV-EPOS/150507_124643/0000//'
 sam["ReggeGribovPartonMC_13TeV-EPOS"]["json"]=''
 sam["ReggeGribovPartonMC_13TeV-EPOS"]["lumiMinBias"]=1.2730583638533814e-05
 sam["ReggeGribovPartonMC_13TeV-EPOS"]["XS"]=78418400000.0
+sam["ReggeGribovPartonMC_13TeV-EPOS"]["pathPAT"]='/XXXTMFPAT/store/user/hvanhaev/ReggeGribovPartonMC_13TeV-EPOS/RunIIWinter15GS_07052015_ReggeGribovPartonMC_13TeV-EPOS/150507_124643/0000//'
 sam["ReggeGribovPartonMC_13TeV-EPOS"]["DS"]='/ReggeGribovPartonMC_13TeV-EPOS/RunIIWinter15GS-castor_MCRUN2_71_V0-v1/GEN-SIM'
 
 sam["ReggeGribovPartonMC_13TeV-QGSJetII"]={}
@@ -69,9 +84,12 @@ sam["ReggeGribovPartonMC_13TeV-QGSJetII"]["GT"]='MCRUN2_71_V0::All'
 sam["ReggeGribovPartonMC_13TeV-QGSJetII"]["name"]='ReggeGribovPartonMC_13TeV-QGSJetII'
 sam["ReggeGribovPartonMC_13TeV-QGSJetII"]["isData"]=False
 sam["ReggeGribovPartonMC_13TeV-QGSJetII"]["numEvents"]=1000000
+sam["ReggeGribovPartonMC_13TeV-QGSJetII"]["pathSE"]='srm://maite.iihe.ac.be:8443/srm/managerv2?SFN=/pnfs/iihe/cms/store/user/hvanhaev/ReggeGribovPartonMC_13TeV-QGSJetII/RunIIWinter15GS_07052015_ReggeGribovPartonMC_13TeV-QGSJetII/150507_124728/0000/'
+sam["ReggeGribovPartonMC_13TeV-QGSJetII"]["pathTrees"]='/XXXTMFTTree/store/user/hvanhaev/ReggeGribovPartonMC_13TeV-QGSJetII/RunIIWinter15GS_07052015_ReggeGribovPartonMC_13TeV-QGSJetII/150507_124728/0000//'
 sam["ReggeGribovPartonMC_13TeV-QGSJetII"]["json"]=''
 sam["ReggeGribovPartonMC_13TeV-QGSJetII"]["lumiMinBias"]=1.2752109198861492e-05
 sam["ReggeGribovPartonMC_13TeV-QGSJetII"]["XS"]=78418400000.0
+sam["ReggeGribovPartonMC_13TeV-QGSJetII"]["pathPAT"]='/XXXTMFPAT/store/user/hvanhaev/ReggeGribovPartonMC_13TeV-QGSJetII/RunIIWinter15GS_07052015_ReggeGribovPartonMC_13TeV-QGSJetII/150507_124728/0000//'
 sam["ReggeGribovPartonMC_13TeV-QGSJetII"]["DS"]='/ReggeGribovPartonMC_13TeV-QGSJetII/RunIIWinter15GS-castor_MCRUN2_71_V0-v1/GEN-SIM'
 
 
