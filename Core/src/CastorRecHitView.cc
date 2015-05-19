07404d38398d6f9c84b801981158064e2eac4ec3
Sebastian <sebastian.baur@kit.edu>
2015-05-19 13:14:12 +0200

    add isBad Quality to RecHits, fhot and sigmaz to CastorJets

--- Core/src/CastorRecHitView.cc
+++ Core/src/CastorRecHitView.cc
@@ -1,6 +1,11 @@
 #include "CommonFSQFramework/Core/interface/CastorRecHitView.h"
 #include "DataFormats/HcalRecHit/interface/CastorRecHit.h"
 #include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "CondFormats/CastorObjects/interface/CastorChannelQuality.h"
+#include "CondFormats/CastorObjects/interface/CastorChannelStatus.h"
+#include "CondFormats/DataRecord/interface/CastorChannelQualityRcd.h"
 
 CastorRecHitView::CastorRecHitView(const edm::ParameterSet& iConfig, TTree * tree):
 EventViewBase(iConfig,  tree)
@@ -8,6 +13,7 @@ EventViewBase(iConfig,  tree)
    registerVecFloat("Energy", tree);
    registerVecInt("Sector", tree);
    registerVecInt("Module", tree);
+   registerVecInt("isBad", tree);
 }
 
 void CastorRecHitView::fillSpecific(const edm::Event& iEvent, const edm::EventSetup& iSetup){
@@ -15,11 +21,30 @@ void CastorRecHitView::fillSpecific(const edm::Event& iEvent, const edm::EventSe
    edm::Handle< edm::SortedCollection<CastorRecHit,edm::StrictWeakOrdering<CastorRecHit> > > castorRecHits;
    iEvent.getByLabel("castorreco",castorRecHits);  
 
+   // retrieve the channel quality lists from database
+   edm::ESHandle<CastorChannelQuality> p;
+   iSetup.get<CastorChannelQualityRcd>().get(p);
+   std::vector<DetId> channels = p->getAllChannels();
+
    // add rechits to tree
     for (unsigned int iRecHit=0; iRecHit < castorRecHits->size(); ++iRecHit) {
         CastorRecHit rh = (*castorRecHits)[iRecHit];
+        HcalCastorDetId castorid = rh.id();
+        DetId genericID=(DetId)castorid;
+
+        bool RechitIsBad = false;
+        for (std::vector<DetId>::iterator channel = channels.begin();channel != channels.end();channel++) {
+            if (channel->rawId() == genericID.rawId()) {
+                // if the rechit is found in the list, mark it bad
+                RechitIsBad=true;
+                break;
+                }
+        }
+
         addToFVec("Energy", rh.energy());
-        addToIVec("Sector", rh.id().sector());
-        addToIVec("Module", rh.id().module());
+        addToIVec("Sector", castorid.sector());
+        addToIVec("Module", castorid.module());
+        addToIVec("isBad", (int)RechitIsBad);
+
     }
 }
