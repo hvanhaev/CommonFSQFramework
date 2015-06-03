d48d2f06937c27957f706fb44382d43033863e39
Hans Van Haevermaet <hans.van.haevermaet@cern.ch>
2015-06-03 17:09:15 +0200

    add new getters for rechits - not tested yet

---
+++ Core/python/CastorRecHitGetter.py
@@ -0,0 +1,13 @@
+import BaseGetter
+
+class CastorRecHitGetter(BaseGetter.BaseGetter):
+    def __init__(self, branchPrefix):
+        BaseGetter.BaseGetter.__init__(self, branchPrefix)
+        #self.knownVariations = set(["_central"])
+        self.knownVariations = set()
+
+    # Note: use the most used branch (so performance wont suffer from reading otherwise unused stuff)
+    def getSize(self):
+        srcBranch = "CastorRecHitEnergy"
+        return getattr(self.chain, srcBranch).size()
+
