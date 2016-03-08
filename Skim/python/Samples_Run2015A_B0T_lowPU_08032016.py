c024ab9237a08c9977b58c19d01ad32df76c65a0
Hans Van Haevermaet <hans.van.haevermaet@cern.ch>
2016-03-08 15:31:01 +0100

    special 0T MC skim with bad HF CaloTowers removed

---
+++ Skim/python/Samples_Run2015A_B0T_lowPU_08032016.py
@@ -0,0 +1,120 @@
+anaVersion="Run2015A_B0T_lowPU_08032016"
+anaType="Run2015A_B0T_lowPU"
+
+cbSmartCommand="smartCopy"
+cbSmartBlackList=""
+cbWMS="https://wmscms.cern.ch:7443/glite_wms_wmproxy_server"
+skimEfficiencyMethod="getSkimEff"
+
+sam = {}
+
+sam["MinBias_TuneEE5C_13TeV-herwigpp_MagnetOff"]={}
+sam["MinBias_TuneEE5C_13TeV-herwigpp_MagnetOff"]["crabJobs"]=10
+sam["MinBias_TuneEE5C_13TeV-herwigpp_MagnetOff"]["GT"]='MCRUN2_740TV0'
+sam["MinBias_TuneEE5C_13TeV-herwigpp_MagnetOff"]["name"]='MinBias_TuneEE5C_13TeV-herwigpp_MagnetOff'
+sam["MinBias_TuneEE5C_13TeV-herwigpp_MagnetOff"]["isData"]=False
+sam["MinBias_TuneEE5C_13TeV-herwigpp_MagnetOff"]["numEvents"]=997682
+sam["MinBias_TuneEE5C_13TeV-herwigpp_MagnetOff"]["pathSE"]='srm://maite.iihe.ac.be:8443/srm/managerv2?SFN=/pnfs/iihe/cms/store/user/hvanhaev/MinBias_TuneEE5C_13TeV-herwigpp/Run2015A_B0T_lowPU_08032016_MinBias_TuneEE5C_13TeV-herwigpp_MagnetOff/160308_102834/0000/'
+sam["MinBias_TuneEE5C_13TeV-herwigpp_MagnetOff"]["pathTrees"]='/XXXTMFTTree/store/user/hvanhaev/MinBias_TuneEE5C_13TeV-herwigpp/Run2015A_B0T_lowPU_08032016_MinBias_TuneEE5C_13TeV-herwigpp_MagnetOff/160308_102834/0000//'
+sam["MinBias_TuneEE5C_13TeV-herwigpp_MagnetOff"]["json"]=''
+sam["MinBias_TuneEE5C_13TeV-herwigpp_MagnetOff"]["lumiMinBias"]=2.7363741086121776e-05
+sam["MinBias_TuneEE5C_13TeV-herwigpp_MagnetOff"]["XS"]=36460000000.0
+sam["MinBias_TuneEE5C_13TeV-herwigpp_MagnetOff"]["pathPAT"]='/XXXTMFPAT/store/user/hvanhaev/MinBias_TuneEE5C_13TeV-herwigpp/Run2015A_B0T_lowPU_08032016_MinBias_TuneEE5C_13TeV-herwigpp_MagnetOff/160308_102834/0000//'
+sam["MinBias_TuneEE5C_13TeV-herwigpp_MagnetOff"]["DS"]='/MinBias_TuneEE5C_13TeV-herwigpp/RunIISpring15DR74-NoPU0TRawReco_magnetOff_MCRUN2_740TV0-v1/GEN-SIM-RECO'
+
+sam["MinBias_TuneMBR_13TeV-pythia8_MagnetOff"]={}
+sam["MinBias_TuneMBR_13TeV-pythia8_MagnetOff"]["crabJobs"]=10
+sam["MinBias_TuneMBR_13TeV-pythia8_MagnetOff"]["GT"]='MCRUN2_740TV0'
+sam["MinBias_TuneMBR_13TeV-pythia8_MagnetOff"]["name"]='MinBias_TuneMBR_13TeV-pythia8_MagnetOff'
+sam["MinBias_TuneMBR_13TeV-pythia8_MagnetOff"]["isData"]=False
+sam["MinBias_TuneMBR_13TeV-pythia8_MagnetOff"]["numEvents"]=997146
+sam["MinBias_TuneMBR_13TeV-pythia8_MagnetOff"]["pathSE"]='srm://maite.iihe.ac.be:8443/srm/managerv2?SFN=/pnfs/iihe/cms/store/user/hvanhaev/MinBias_TuneMBR_13TeV-pythia8/Run2015A_B0T_lowPU_08032016_MinBias_TuneMBR_13TeV-pythia8_MagnetOff/160308_102946/0000/'
+sam["MinBias_TuneMBR_13TeV-pythia8_MagnetOff"]["pathTrees"]='/XXXTMFTTree/store/user/hvanhaev/MinBias_TuneMBR_13TeV-pythia8/Run2015A_B0T_lowPU_08032016_MinBias_TuneMBR_13TeV-pythia8_MagnetOff/160308_102946/0000//'
+sam["MinBias_TuneMBR_13TeV-pythia8_MagnetOff"]["json"]=''
+sam["MinBias_TuneMBR_13TeV-pythia8_MagnetOff"]["lumiMinBias"]=1.271571467920794e-05
+sam["MinBias_TuneMBR_13TeV-pythia8_MagnetOff"]["XS"]=78418400000.0
+sam["MinBias_TuneMBR_13TeV-pythia8_MagnetOff"]["pathPAT"]='/XXXTMFPAT/store/user/hvanhaev/MinBias_TuneMBR_13TeV-pythia8/Run2015A_B0T_lowPU_08032016_MinBias_TuneMBR_13TeV-pythia8_MagnetOff/160308_102946/0000//'
+sam["MinBias_TuneMBR_13TeV-pythia8_MagnetOff"]["DS"]='/MinBias_TuneMBR_13TeV-pythia8/RunIISpring15DR74-NoPU0TRawReco_magnetOff_MCRUN2_740TV0-v1/GEN-SIM-RECO'
+
+sam["MinBias_TuneMonash13_13TeV-pythia8_MagnetOff"]={}
+sam["MinBias_TuneMonash13_13TeV-pythia8_MagnetOff"]["crabJobs"]=10
+sam["MinBias_TuneMonash13_13TeV-pythia8_MagnetOff"]["GT"]='MCRUN2_740TV0'
+sam["MinBias_TuneMonash13_13TeV-pythia8_MagnetOff"]["name"]='MinBias_TuneMonash13_13TeV-pythia8_MagnetOff'
+sam["MinBias_TuneMonash13_13TeV-pythia8_MagnetOff"]["isData"]=False
+sam["MinBias_TuneMonash13_13TeV-pythia8_MagnetOff"]["numEvents"]=953393
+sam["MinBias_TuneMonash13_13TeV-pythia8_MagnetOff"]["pathSE"]='srm://maite.iihe.ac.be:8443/srm/managerv2?SFN=/pnfs/iihe/cms/store/user/hvanhaev/MinBias_TuneMonash13_13TeV-pythia8/Run2015A_B0T_lowPU_08032016_MinBias_TuneMonash13_13TeV-pythia8_MagnetOff/160308_102558/0000/'
+sam["MinBias_TuneMonash13_13TeV-pythia8_MagnetOff"]["pathTrees"]='/XXXTMFTTree/store/user/hvanhaev/MinBias_TuneMonash13_13TeV-pythia8/Run2015A_B0T_lowPU_08032016_MinBias_TuneMonash13_13TeV-pythia8_MagnetOff/160308_102558/0000//'
+sam["MinBias_TuneMonash13_13TeV-pythia8_MagnetOff"]["json"]=''
+sam["MinBias_TuneMonash13_13TeV-pythia8_MagnetOff"]["lumiMinBias"]=1.2157771645430154e-05
+sam["MinBias_TuneMonash13_13TeV-pythia8_MagnetOff"]["XS"]=78418400000.0
+sam["MinBias_TuneMonash13_13TeV-pythia8_MagnetOff"]["pathPAT"]='/XXXTMFPAT/store/user/hvanhaev/MinBias_TuneMonash13_13TeV-pythia8/Run2015A_B0T_lowPU_08032016_MinBias_TuneMonash13_13TeV-pythia8_MagnetOff/160308_102558/0000//'
+sam["MinBias_TuneMonash13_13TeV-pythia8_MagnetOff"]["DS"]='/MinBias_TuneMonash13_13TeV-pythia8/RunIISpring15DR74-NoPU0TRawReco_magnetOff_MCRUN2_740TV0-v1/GEN-SIM-RECO'
+
+sam["MinBias_TuneZ2star_13TeV-pythia6_MagnetOff"]={}
+sam["MinBias_TuneZ2star_13TeV-pythia6_MagnetOff"]["crabJobs"]=10
+sam["MinBias_TuneZ2star_13TeV-pythia6_MagnetOff"]["GT"]='MCRUN2_740TV0'
+sam["MinBias_TuneZ2star_13TeV-pythia6_MagnetOff"]["name"]='MinBias_TuneZ2star_13TeV-pythia6_MagnetOff'
+sam["MinBias_TuneZ2star_13TeV-pythia6_MagnetOff"]["isData"]=False
+sam["MinBias_TuneZ2star_13TeV-pythia6_MagnetOff"]["numEvents"]=998082
+sam["MinBias_TuneZ2star_13TeV-pythia6_MagnetOff"]["pathSE"]='srm://maite.iihe.ac.be:8443/srm/managerv2?SFN=/pnfs/iihe/cms/store/user/hvanhaev/MinBias_TuneZ2star_13TeV-pythia6/Run2015A_B0T_lowPU_08032016_MinBias_TuneZ2star_13TeV-pythia6_MagnetOff/160308_102514/0000/'
+sam["MinBias_TuneZ2star_13TeV-pythia6_MagnetOff"]["pathTrees"]='/XXXTMFTTree/store/user/hvanhaev/MinBias_TuneZ2star_13TeV-pythia6/Run2015A_B0T_lowPU_08032016_MinBias_TuneZ2star_13TeV-pythia6_MagnetOff/160308_102514/0000//'
+sam["MinBias_TuneZ2star_13TeV-pythia6_MagnetOff"]["json"]=''
+sam["MinBias_TuneZ2star_13TeV-pythia6_MagnetOff"]["lumiMinBias"]=1.2753411704574495e-05
+sam["MinBias_TuneZ2star_13TeV-pythia6_MagnetOff"]["XS"]=78260000000.0
+sam["MinBias_TuneZ2star_13TeV-pythia6_MagnetOff"]["pathPAT"]='/XXXTMFPAT/store/user/hvanhaev/MinBias_TuneZ2star_13TeV-pythia6/Run2015A_B0T_lowPU_08032016_MinBias_TuneZ2star_13TeV-pythia6_MagnetOff/160308_102514/0000//'
+sam["MinBias_TuneZ2star_13TeV-pythia6_MagnetOff"]["DS"]='/MinBias_TuneZ2star_13TeV-pythia6/RunIISpring15DR74-NoPU0TRawReco_magnetOff_MCRUN2_740TV0-v1/GEN-SIM-RECO'
+
+sam["ReggeGribovPartonMC_13TeV-EPOS_MagnetOff"]={}
+sam["ReggeGribovPartonMC_13TeV-EPOS_MagnetOff"]["crabJobs"]=10
+sam["ReggeGribovPartonMC_13TeV-EPOS_MagnetOff"]["GT"]='MCRUN2_740TV0'
+sam["ReggeGribovPartonMC_13TeV-EPOS_MagnetOff"]["name"]='ReggeGribovPartonMC_13TeV-EPOS_MagnetOff'
+sam["ReggeGribovPartonMC_13TeV-EPOS_MagnetOff"]["isData"]=False
+sam["ReggeGribovPartonMC_13TeV-EPOS_MagnetOff"]["numEvents"]=998671
+sam["ReggeGribovPartonMC_13TeV-EPOS_MagnetOff"]["pathSE"]='srm://maite.iihe.ac.be:8443/srm/managerv2?SFN=/pnfs/iihe/cms/store/user/hvanhaev/ReggeGribovPartonMC_13TeV-EPOS/Run2015A_B0T_lowPU_08032016_ReggeGribovPartonMC_13TeV-EPOS_MagnetOff/160308_102645/0000/'
+sam["ReggeGribovPartonMC_13TeV-EPOS_MagnetOff"]["pathTrees"]='/XXXTMFTTree/store/user/hvanhaev/ReggeGribovPartonMC_13TeV-EPOS/Run2015A_B0T_lowPU_08032016_ReggeGribovPartonMC_13TeV-EPOS_MagnetOff/160308_102645/0000//'
+sam["ReggeGribovPartonMC_13TeV-EPOS_MagnetOff"]["json"]=''
+sam["ReggeGribovPartonMC_13TeV-EPOS_MagnetOff"]["lumiMinBias"]=1.2735161645736204e-05
+sam["ReggeGribovPartonMC_13TeV-EPOS_MagnetOff"]["XS"]=78418400000.0
+sam["ReggeGribovPartonMC_13TeV-EPOS_MagnetOff"]["pathPAT"]='/XXXTMFPAT/store/user/hvanhaev/ReggeGribovPartonMC_13TeV-EPOS/Run2015A_B0T_lowPU_08032016_ReggeGribovPartonMC_13TeV-EPOS_MagnetOff/160308_102645/0000//'
+sam["ReggeGribovPartonMC_13TeV-EPOS_MagnetOff"]["DS"]='/ReggeGribovPartonMC_13TeV-EPOS/RunIISpring15DR74-NoPU0TRawReco_magnetOff_MCRUN2_740TV0-v1/GEN-SIM-RECO'
+
+sam["ReggeGribovPartonMC_13TeV-QGSJetII_MagnetOff"]={}
+sam["ReggeGribovPartonMC_13TeV-QGSJetII_MagnetOff"]["crabJobs"]=10
+sam["ReggeGribovPartonMC_13TeV-QGSJetII_MagnetOff"]["GT"]='MCRUN2_740TV0'
+sam["ReggeGribovPartonMC_13TeV-QGSJetII_MagnetOff"]["name"]='ReggeGribovPartonMC_13TeV-QGSJetII_MagnetOff'
+sam["ReggeGribovPartonMC_13TeV-QGSJetII_MagnetOff"]["isData"]=False
+sam["ReggeGribovPartonMC_13TeV-QGSJetII_MagnetOff"]["numEvents"]=980585
+sam["ReggeGribovPartonMC_13TeV-QGSJetII_MagnetOff"]["pathSE"]='srm://maite.iihe.ac.be:8443/srm/managerv2?SFN=/pnfs/iihe/cms/store/user/hvanhaev/ReggeGribovPartonMC_13TeV-QGSJetII/Run2015A_B0T_lowPU_08032016_ReggeGribovPartonMC_13TeV-QGSJetII_MagnetOff/160308_102733/0000/'
+sam["ReggeGribovPartonMC_13TeV-QGSJetII_MagnetOff"]["pathTrees"]='/XXXTMFTTree/store/user/hvanhaev/ReggeGribovPartonMC_13TeV-QGSJetII/Run2015A_B0T_lowPU_08032016_ReggeGribovPartonMC_13TeV-QGSJetII_MagnetOff/160308_102733/0000//'
+sam["ReggeGribovPartonMC_13TeV-QGSJetII_MagnetOff"]["json"]=''
+sam["ReggeGribovPartonMC_13TeV-QGSJetII_MagnetOff"]["lumiMinBias"]=1.2504526998765596e-05
+sam["ReggeGribovPartonMC_13TeV-QGSJetII_MagnetOff"]["XS"]=78418400000.0
+sam["ReggeGribovPartonMC_13TeV-QGSJetII_MagnetOff"]["pathPAT"]='/XXXTMFPAT/store/user/hvanhaev/ReggeGribovPartonMC_13TeV-QGSJetII/Run2015A_B0T_lowPU_08032016_ReggeGribovPartonMC_13TeV-QGSJetII_MagnetOff/160308_102733/0000//'
+sam["ReggeGribovPartonMC_13TeV-QGSJetII_MagnetOff"]["DS"]='/ReggeGribovPartonMC_13TeV-QGSJetII/RunIISpring15DR74-NoPU0TRawReco_magnetOff_MCRUN2_740TV0-v1/GEN-SIM-RECO'
+
+
+def fixLocalPaths(sam):
+        import os,imp
+        if "SmallXAnaDefFile" not in os.environ:
+            print "Please set SmallXAnaDefFile environment variable:"
+            print "export SmallXAnaDefFile=FullPathToFile"
+            raise Exception("Whooops! SmallXAnaDefFile env var not defined")
+
+        anaDefFile = os.environ["SmallXAnaDefFile"]
+        mod_dir, filename = os.path.split(anaDefFile)
+        mod, ext = os.path.splitext(filename)
+        f, filename, desc = imp.find_module(mod, [mod_dir])
+        mod = imp.load_module(mod, f, filename, desc)
+
+        localBasePathPAT = mod.PATbasePATH
+        localBasePathTrees = mod.TTreeBasePATH
+
+        for s in sam:
+            if "pathPAT" in sam[s]:
+                sam[s]["pathPAT"] = sam[s]["pathPAT"].replace("XXXTMFPAT", localBasePathPAT)
+            if "pathTrees" in sam[s]:
+                sam[s]["pathTrees"] = sam[s]["pathTrees"].replace("XXXTMFTTree", localBasePathTrees)
+            #print sam[s]["pathPAT"]
+            #print sam[s]["pathTrees"]
+        return sam
+sam = fixLocalPaths(sam)
