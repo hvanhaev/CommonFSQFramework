03489e8ddfc6d87e2dc3ce335804000ea8778890
R Ulrich <ralf.ulrich@kit.edu>
2017-09-13 12:42:19 +0200

    small fix

--- Core/python/TriggerResultsViewsConfigs.py
+++ Core/python/TriggerResultsViewsConfigs.py
@@ -46,7 +46,7 @@ def get(todo):
         miniView = cms.string("TriggerResultsView"),
         branchPrefix = cms.untracked.string("trgl1"),
         process = cms.string("HLT"),
-        isStage1 = cms.bool(False),
+        isStage1 = cms.bool(True),
         storePrescales = cms.bool(False),
         triggers = cms.vstring("L1GTTech","L1GTAlgo")
     )
@@ -91,7 +91,7 @@ def get(todo):
        triggers = cms.vstring("FullTrack12"),
        FullTrack12 = cms.vstring("HLT_FullTrack12ForEndOfFill_v1")
     )
-
+    
     defs["FullTrackTriggerResultsViewWithPS"]  = cms.PSet(
        miniView = cms.string("TriggerResultsView"),
        branchPrefix = cms.untracked.string("trgTracks"),
@@ -100,7 +100,7 @@ def get(todo):
        triggers = cms.vstring("FullTrack12"),
        FullTrack12 = cms.vstring("HLT_FullTrack12ForEndOfFill_v1")
     )
-
+    
     defs["CastorSpecialJetTriggerResultsView"]  = cms.PSet(
         miniView = cms.string("TriggerResultsView"),
         branchPrefix = cms.untracked.string("CasTrg"),
@@ -114,20 +114,20 @@ def get(todo):
         CastorHighJet = cms.vstring("HLT_L1CastorHighJet_v*"),
         CastorDiJet = cms.vstring("HLT_L1CastorMediumJet_PFJet15_v*")
     )
-
+    
     defs["CastorPATriggerResultsView"]  = cms.PSet(
         miniView = cms.string("TriggerResultsView"),
         branchPrefix = cms.untracked.string("CasPATrg"),
         process = cms.string("HLT"),
-        isStage1 = cms.bool(False),
+        isStage1 = cms.bool(True),
         storePrescales = cms.bool(False),
         triggers = cms.vstring("CastorPAMedJet","CastorPAMuon","Random"),
         CastorPAMedJet = cms.vstring("HLT_PAL1CastorMediumJet_BptxAND_v*"),
         CastorPAMuon = cms.vstring("HLT_PAL1CastorHaloMuon_v*"),
         Random = cms.vstring("HLT_Random*")
     )
-
- 
+    
+    
     # main function
     ret = {}
     for t in todo:
