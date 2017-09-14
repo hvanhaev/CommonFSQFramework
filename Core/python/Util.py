f63cfc03eb590b76988bdff78295540fce843915
ralfulrich <ralf.ulrich@kit.edu>
2017-09-14 22:10:26 +0200

    unified gfal-ls calls. Allow for root trees in subdirectories

--- Core/python/Util.py
+++ Core/python/Util.py
@@ -1,21 +1,20 @@
 import os, sys, subprocess, imp
 
+
+
 def getCrabVersion():
     try:
         #ver=subprocess.check_output(["crab", "--version"])#,, "v3"
         # python 2.6 compat
         p = subprocess.Popen(["crab", "--version"], stdout=subprocess.PIPE)
         ver = p.communicate()[0]
-
-
     except OSError:
         raise Exception("Seems that crab environment is not defined. Exit... stage left")
-
     ret = 2
     if "v3" in ver:
         ret = 3 
-
     return ret
+
 
 
 def getVariant():
@@ -23,15 +22,18 @@ def getVariant():
         m = " Cannot get ana variant from env. Set SmallXAnaVersion "
         m += "to desired value (e.g. by sourcing one of files from CommonFSQFramework.Core/env/ directory) "
         raise Exception(m)
-
     variant = os.environ["SmallXAnaVersion"]
     return variant
+
+
 
 def getFullPathToAnaDefinitionFile():
     variant = getVariant()
     command = "import "+variant+" as tmpxxx"
     exec command
     return tmpxxx.__file__
+
+
 
 def getAnaDefinition(varname, toGlobal=False):
     variant = getVariant()
@@ -40,48 +42,44 @@ def getAnaDefinition(varname, toGlobal=False):
         exec(command, globals(), globals())
     else:
         exec(command)
-
     obj = eval(varname)
-
     return obj
 
+
+
 def getROOTPrefix():
-
     if "SmallXAnaDefFile" not in os.environ:
         print "Please set SmallXAnaDefFile environment variable:"
         print "export SmallXAnaDefFile=FullPathToFile"
         raise Exception("Whooops! SmallXAnaDefFile env var not defined")
-
     anaDefFile = os.environ["SmallXAnaDefFile"]
     mod_dir, filename = os.path.split(anaDefFile)
     mod, ext = os.path.splitext(filename)
     f, filename, desc = imp.find_module(mod, [mod_dir])
-    mod = imp.load_module(mod, f, filename, desc)
-    
+    mod = imp.load_module(mod, f, filename, desc)    
     return mod.ROOTPrefix
-    
-def getTTreeBasePath():
 
+
+
+def getTTreeBasePath():
     if "SmallXAnaDefFile" not in os.environ:
         print "Please set SmallXAnaDefFile environment variable:"
         print "export SmallXAnaDefFile=FullPathToFile"
         raise Exception("Whooops! SmallXAnaDefFile env var not defined")
-
     anaDefFile = os.environ["SmallXAnaDefFile"]
     mod_dir, filename = os.path.split(anaDefFile)
     mod, ext = os.path.splitext(filename)
     f, filename, desc = imp.find_module(mod, [mod_dir])
     mod = imp.load_module(mod, f, filename, desc)
-
     return mod.TTreeBasePATH
-   
-def getPATBasePath():
 
+
+
+def getPATBasePath():
     if "SmallXAnaDefFile" not in os.environ:
         print "Please set SmallXAnaDefFile environment variable:"
         print "export SmallXAnaDefFile=FullPathToFile"
         raise Exception("Whooops! SmallXAnaDefFile env var not defined")
-
     anaDefFile = os.environ["SmallXAnaDefFile"]
     mod_dir, filename = os.path.split(anaDefFile)
     mod, ext = os.path.splitext(filename)
@@ -90,6 +88,8 @@ def getPATBasePath():
 
     return mod.PATbasePATH
 
+
+
 def preventExit():
     print " "
     py3 = sys.version_info[0] > 2 #creates boolean value for test that Python major version > 2
@@ -97,5 +97,37 @@ def preventExit():
         response = input("Press enter to exit ")
     else:
         response = raw_input("Press enter to exit ")
-
     return 1
+
+
+
+def getFileListGFAL(path, subdir=''):
+    ret = []
+    cnt = 0
+    if not  distutils.spawn.find_executable("gfal-ls"):
+        raise Exception("Cannot find gfal-ls executable. Check your grid environment!")
+    command = ["gfal-ls", "-a", path + subdir]
+    lineCnt = 0
+    print "Obtaining file list for ", path, " with gfal"
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+    for line in iter(proc.stdout.readline,''):
+        lineCnt += 1
+        l =  line.strip()
+        fname = l.split("/")[-1]
+        if not fname.endswith(".root"):
+            # could be directory...
+            ret2 = getFileListGFAL(path, '/' + fname)
+            ret += ret2
+            cnt += len(ret2)
+            continue
+        srcFile = path + "/" + fname
+        cnt += 1
+        ret.append(srcFile)
+    if lineCnt < 1:
+        err = "Cannot get filelist for  " + path + "\n"
+        err += " - if  some files were copied allready this probably means some server related problems."
+        err += " Please retry in couple of minutes. \n"  
+        err += " - if none of the files were copied please check your certificate proxy.\n"
+        print err
+#        raise Exception(err)
+    return ret
--- Core/scripts/copyAnaData.py
+++ Core/scripts/copyAnaData.py
@@ -14,118 +14,6 @@ import subprocess
 import CommonFSQFramework.Core.Util
 
 
-def getFileListLcgLs(path):
-    ret = []
-    command = ["lcg-ls", path]
-    proc = subprocess.Popen(command, stdout=subprocess.PIPE)
-    cnt = 0
-    for line in iter(proc.stdout.readline,''):
-        l = line.strip()
-        fname = l.split("/")[-1]
-        if ".root" not in fname: continue
-        srcFile = path + "/" + fname
-        #targetFile = targetDir + "/" + fname
-        ret.append(srcFile)
-
-    return ret
-
-def getFileListSrmLS(path):
-    maxResults = 500
-    offset = 0
-
-    ret = []
-    lastTime = 0
-    cnt = 0
-    while True: # handle maxResults results at a time
-        command = ["srmls", "-2", "--offset", str(offset), "--count", str(maxResults),  path]
-        retryCnt = 1
-        goodRootFiles = 0
-        # for current offset value obtain list of files.
-        #    Try couple of times to handle empty output of srmls for some calls
-        while True:
-            lineCnt = 0
-            print "Obtaining file list for", path, "- try", retryCnt, "offset", offset
-
-            curTime = time.time()
-            sinceLastSrmls= abs(curTime-lastTime)
-            if sinceLastSrmls < 15: # dont be too agressive
-                print "   Since last srmls", sinceLastSrmls,"- sleeping"
-                time.sleep( int(15 - sinceLastSrmls) )
-                print "   OK, back to work"
-            proc = subprocess.Popen(command, stdout=subprocess.PIPE)
-            lastTime = int(time.time())
-            for line in iter(proc.stdout.readline,''):
-                lineCnt += 1
-                #print "XAQ", line.rstrip()
-                l =  line.strip()
-                fname = l.split("/")[-1]
-                if not fname.endswith(".root"): continue
-                goodRootFiles += 1 # this is a bit risky, since we may have other files in dir then rootfiles. TODO
-                #print "Found", fname
-
-
-                srcFile = path + "/" + fname
-                #targetFile = targetDir + "/" + fname
-
-                #print srcFile, targetFile
-                cnt += 1
-
-                
-                #ret[srcFile]=targetFile
-                ret.append(srcFile)
-
-            if lineCnt <= 1:
-                if retryCnt == 10:
-                    err = "Cannot get filelist for  "+path+"\n"
-                    err += " - if  some files were copied allready this probably means some server related problems."
-                    err += " Please retry in couple of minutes. \n"  
-                    err += " - if none of the files were copied please check your certificate proxy.\n"
-                    raise Exception(err)
-
-                retryCnt += 1
-            else:
-                break
-
-        if goodRootFiles>0:
-            offset += maxResults
-        else:
-            break
-
-    return ret
-
-
-def getFileListGFAL(path):
-    ret = []
-    cnt = 0
-
-    command = ["gfal-ls", "-a", path]
-    # for current offset value obtain list of files.
-    #    Try couple of times to handle empty output of srmls for some calls
-
-    lineCnt = 0
-    print "Obtaining file list for ", path, " with gfal"
-    proc = subprocess.Popen(command, stdout=subprocess.PIPE)
-
-    for line in iter(proc.stdout.readline,''):
-        lineCnt += 1
-        l =  line.strip()
-        fname = l.split("/")[-1]
-        if not fname.endswith(".root"): continue
-
-        srcFile = path + "/" + fname
-        cnt += 1
-        ret.append(srcFile)
-
-    if lineCnt < 1:
-        err = "Cannot get filelist for  " + path + "\n"
-        err += " - if  some files were copied allready this probably means some server related problems."
-        err += " Please retry in couple of minutes. \n"  
-        err += " - if none of the files were copied please check your certificate proxy.\n"
-        print err
-        raise Exception(err)
-
-    return ret
-
 
 def checkRootFile(fp):
     while "//" in fp:
@@ -157,6 +45,7 @@ def checkRootFile(fp):
     ret = proc.poll()
     return ret
     
+
 
 def checkDataIntegrity(remove = False, checkFilesWithRoot = False):
 
@@ -257,23 +146,9 @@ def main():
     parser.add_option("-c", "--checkDataIntegrity", action="store_true",  dest="check")
     parser.add_option("-d", "--deleteBadFiles", action="store_true",  dest="remove")
     parser.add_option("-r", "--rootCheck", action="store_true",  dest="checkFilesWithRoot")
-    parser.add_option("-s", "--srmls", action="store_true",  dest="usesrmls")
-    parser.add_option("-g", "--gfal", action="store_true",  dest="useGFAL")
-    parser.add_option("-l", "--lcg", action="store_true",  dest="useLCG")
     parser.add_option("-m", "--maxFilesMC", action="store",  type="int", dest="maxFilesMC")
     (options, args) = parser.parse_args()
 
-    c_method=0
-    if options.usesrmls:
-        c_method += 1
-    if options.useGFAL:
-        c_method += 1
-    if options.useLCG:
-        c_method += 1
-
-    if c_method==0 or c_method>1:
-        print "You must specify exactly one of -s -g or -l"
-        sys.exit(1)
 
     maxFilesMC = -1
     if options.maxFilesMC:
@@ -332,28 +207,32 @@ def main():
 
         cntSamples += 1
 
-        # TODO: check dir existence
-
-        # on my installation lcg-ls does not have offset/count params
-        # needed for srm access to dirs with >1000 files.
-        
-#command = ["lcg-ls", sampleList[s]["pathSE"]]
-        if options.useGFAL:
-            flist = getFileListGFAL(sampleList[s]["pathSE"])
-        elif options.usesrmls:
-            flist = getFileListSrmLS(sampleList[s]["pathSE"])
-        else:
-            flist = getFileListLcgLs(sampleList[s]["pathSE"])
+        flist = CommonFSQFramework.Core.Util.getFileListGFAL(sampleList[s]["pathSE"])
 
         cntCopy = 0
         cntRead = 0
+        createdDirs = []
         for srcFile in flist:
             cntRead += 1
             cntReadSum += 1
-            fname = srcFile.split("/")[-1]
+            path_components = srcFile.split("/")
+            fname = path_components[-1] # filename
             patFile = "mnTrgAna_PAT_" in fname
             treeFile = "trees_" in fname
-
+            
+            # check eventual sub-paths
+            subdir = ''
+            if (len(path_components)>1):
+                for isub in range(len(path_components)-1):
+                    subdir += path_components[isub] + '/'
+                    if (subdir not in createdDirs):
+                        mkdirCmd = ['gfal-mkdir', path+'/'+ subdir]
+                        mkdirCmdOut = Popen(mkdirCmd)
+                        #t = out.communicate()[0],
+                        if (mkdirCmdOut.returncode!=0):
+                            print ("error", str(mkdirCmd), str(mkdirCmd.returncode))
+                            sys.exit(1)                        
+                        
             doCopy = False
             if patFile and doPAT:
                 doCopy = True
@@ -365,20 +244,13 @@ def main():
                 typeString = "treeFile"
 
             if not doCopy: continue
-            targetFile = targetDir + "/" + fname
+            targetFile = targetDir + "/" + subdir + fname
             if not sampleList[s]["isData"] and maxFilesMC >= 0 and cntCopy >= maxFilesMC:
                 continue
 
             cntCopy += 1
 
-            if options.useGFAL:
-                cpCommand = ['gfal-copy', srcFile, targetFile]
-            else:
-                if "eos/cms" in targetDir:
-                    cpCommand = ['lcg-cp', srcFile, "srm://srm-eoscms.cern.ch/"+targetFile]
-                else:
-                    cpCommand = ['lcg-cp', srcFile, targetFile]
-            
+            cpCommand = ['gfal-copy', srcFile, targetFile]            
 	    #cpCommand = ['lcg-ls', srcFile]
 	    #print "would be cpCommand: ", cpCommand
             
