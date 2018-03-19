39fa3514369062a0d103e6fc0c7070548df8ad6e
maximpieters <mpieters@cern.ch>
2018-03-19 13:38:34 +0100

    Updated file

--- Core/interface/CastorRecHitView.h
+++ Core/interface/CastorRecHitView.h
@@ -11,7 +11,6 @@ class CastorRecHitView: public EventViewBase {
       virtual void fillSpecific(const edm::Event&, const edm::EventSetup&);
       bool m_onlyGoodRecHits;
       bool m_saturationInfo;
-      edm::InputTag m_inputlabel;
 };
 
 #endif
